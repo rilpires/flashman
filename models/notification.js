b4746a8214659755f3b99c7ab2a923695afa8294
Guilherme Senges <guilherme@anlix.io>
2018-12-07 00:29:53 -0200

    Added fields on notification model

--- models/notification.js
+++ models/notification.js
@@ -2,31 +2,38 @@
 const mongoose = require('mongoose');
 
 let notificationSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, 'Ops! Insira o título da notificação.'],
-  },
   created_at: {type: Date, default: new Date()},
   message: {
     type: String,
     required: [true, 'Ops! Insira a mensagem.'],
+  },
+  // Code makes each notification message unique
+  message_code: {
+    type: Number,
+    enum: [1, // MQTT secret doesn't match
+    ],
+    required: true,
   },
   severity: {
     type: String,
     enum: ['info', 'warning', 'alert', 'danger'],
     required: [true, 'Ops! Insira a severidade da notificação.'],
   },
+  // Type will help with roles permissions
   type: {
     type: String,
     enum: ['communication', 'wan-network', 'lan-network'],
     required: [true, 'Ops! Insira o tipo desta notificação.'],
   },
-  action_title: {
-    type: String,
+  // Notification can happen more than once
+  allow_duplicate: {
+    type: Boolean,
+    required: true,
+    default: true,
   },
-  action_url: {
-    type: String,
-  },
+  action_title: String,
+  action_url: String,
+  target: String,
 });
 
 let Notification = mongoose.model('Notification', notificationSchema);
