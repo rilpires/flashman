f3092f2144ac74e130508dfa9fa45785e79508ca
Guilherme Senges <guilherme@anlix.io>
2019-02-08 20:00:09 -0200

    Refactored log modal and included export button

--- public/javascripts/show_devices_logs_actions.js
+++ public/javascripts/show_devices_logs_actions.js
@@ -1,4 +1,7 @@
 
+// Store log to be downloadable
+let logBodyRawContent = '';
+
 // Important: include and initialize socket.io first using socket var
 socket.on('LIVELOG', function(macaddr, data) {
   if (($('#analyse-logs').data('bs.modal') || {})._isShown) {
@@ -7,7 +10,10 @@ socket.on('LIVELOG', function(macaddr, data) {
       let textarea = $('#logArea');
       if (textarea.text() == 'Aguardando resposta do roteador...') {
         let usrtypes = ['user', 'daemon', 'kern', 'local1', 'authpriv'];
-        textarea.html('<code>' + pako.ungzip(data, {to: 'string'}) + '</code>');
+        let logContent = pako.ungzip(data, {to: 'string'});
+        // Store log to be downloadable
+        logBodyRawContent = logContent;
+        textarea.html('<code>' + logContent + '</code>');
         textarea.highlight(
           usrtypes.map(function(x) {
             return x + '.warn';
@@ -26,6 +32,8 @@ socket.on('LIVELOG', function(macaddr, data) {
           }),
           {element: 'strong', className: 'text-info'}
         );
+        // Enable export button
+        $('#export-log').removeClass('disabled');
       }
     }
   }
@@ -43,6 +51,8 @@ let printLogData = function(url) {
       if (ct.indexOf('json') > -1) {
         textarea.html('Erro: ' + res.message);
       } else {
+        // Store log to be downloadable
+        logBodyRawContent = res;
         textarea.html('<code>' + res + '</code>');
         textarea.highlight(
           usrtypes.map(function(x) {
@@ -62,12 +72,38 @@ let printLogData = function(url) {
           }),
           {element: 'strong', className: 'text-info'}
         );
+        // Enable export button
+        $('#export-log').removeClass('disabled');
       }
     },
     error: function(xhr, status, error) {
       textarea.html(status + ' ' + error);
     },
   });
+};
+
+let downloadFile = function(body, filename) {
+  let textFile;
+  let downloadLink;
+  // File content
+  textFile = new Blob([body], {type: 'text/text'});
+  // Download link
+  downloadLink = document.createElement('a');
+  // File name
+  downloadLink.download = filename;
+  // Create a link to the file
+  downloadLink.href = window.URL.createObjectURL(textFile);
+  // Hide download link
+  downloadLink.style.display = 'none';
+  // Add the link to DOM
+  document.body.appendChild(downloadLink);
+  // Click download link
+  downloadLink.click();
+};
+
+let exportLogToFile = function(filename) {
+  // Download log file
+  downloadFile(logBodyRawContent, filename);
 };
 
 $(document).ready(function() {
@@ -119,9 +155,14 @@ $(document).ready(function() {
     });
   });
 
-  // Restore placeholder
+  // Restore default modal state
   $('#analyse-logs').on('hidden.bs.modal', function() {
     $('#logs-placeholder').show();
     $('#logArea').hide();
+    $('#export-log').addClass('disabled');
+  });
+
+  $('#export-log').click(function(event) {
+    exportLogToFile('log.txt');
   });
 });
