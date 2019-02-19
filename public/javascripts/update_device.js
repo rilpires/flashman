38b3d385d6588a3e433f801a827c600f5737b93b
Guilherme Senges <guilherme@anlix.io>
2019-02-19 00:41:50 -0300

    Added swal notifications before firmware upgrade

--- public/javascripts/update_device.js
+++ public/javascripts/update_device.js
@@ -6,49 +6,66 @@ let updateAllDevices = function(event) {
   let singleReleases = $('#all-devices .dropdown-toggle').data('singlereleases');
   let selectedRelease = $(this).text();
   let selectedModels = [];
-  // Change current selected release
-  $('#all-devices').find('span.selected').text(selectedRelease);
 
-  // Get models of selected release
-  for (let i=0; i < singleReleases.length; i++) {
-    if (singleReleases[i].id == selectedRelease) {
-      selectedModels = singleReleases[i].model;
-      break;
-    }
-  }
+  swal({
+    type: 'warning',
+    title: 'Atenção!',
+    text: 'Todos os roteadores desta página com a release ' +
+    'selecionada serão marcados para atualização. Lembre-se de notificar ' +
+    'seus usuários antes de prosseguir.',
+    confirmButtonText: 'Prosseguir',
+    confirmButtonColor: '#4db6ac',
+    cancelButtonText: 'Cancelar',
+    cancelButtonColor: '#f2ab63',
+    showCancelButton: true,
+  }).then(function(result) {
+    if (result.value) {
+      // Change current selected release
+      $('#all-devices').find('span.selected').text(selectedRelease);
 
-  rows.each(function(idx) {
-    let deviceModel = $(this).data('deviceModel');
-    if (deviceModel) {
-      if (deviceModel.includes('N/')) {
-        deviceModel = deviceModel.replace('N/', '');
+      // Get models of selected release
+      for (let i=0; i < singleReleases.length; i++) {
+        if (singleReleases[i].id == selectedRelease) {
+          selectedModels = singleReleases[i].model;
+          break;
+        }
       }
-      if ($(this).prop('id') !== undefined && $(this).prop('id').length > 0 &&
-          selectedModels.includes(deviceModel)) {
-        let id = $(this).prop('id');
-        let rel = selectedRelease;
-        idsObj[id] = rel;
-      }
-    }
-  });
 
-  $.post('/devicelist/updateall',
-         {content: JSON.stringify({ids: idsObj, do_update: true})})
-  .done(function(res) {
-    if (res.success) {
-      res.devices.forEach(function(deviceId) {
-        let selBtnGroup = $('#' + $.escapeSelector(deviceId))
-          .find('.btn-group .btn-group');
-        selBtnGroup.find('span.selected').text(selectedRelease);
-        // Deactivate dropdown button
-        selBtnGroup.find('.dropdown-toggle').attr('disabled', true);
-        // Activate waiting status
-        let upgradeStatus = selBtnGroup.find('span.upgrade-status');
-        upgradeStatus.find('.status-none').addClass('d-none');
-        upgradeStatus.find('.status-waiting').removeClass('d-none');
-        // Activate cancel button
-        selBtnGroup.siblings('.btn-cancel-update')
-          .addClass('btn-danger').attr('disabled', false);
+      rows.each(function(idx) {
+        let deviceModel = $(this).data('deviceModel');
+        if (deviceModel) {
+          if (deviceModel.includes('N/')) {
+            deviceModel = deviceModel.replace('N/', '');
+          }
+          if ($(this).prop('id') !== undefined &&
+              $(this).prop('id').length > 0 &&
+              selectedModels.includes(deviceModel)) {
+            let id = $(this).prop('id');
+            let rel = selectedRelease;
+            idsObj[id] = rel;
+          }
+        }
+      });
+
+      $.post('/devicelist/updateall',
+             {content: JSON.stringify({ids: idsObj, do_update: true})})
+      .done(function(res) {
+        if (res.success) {
+          res.devices.forEach(function(deviceId) {
+            let selBtnGroup = $('#' + $.escapeSelector(deviceId))
+              .find('.btn-group .btn-group');
+            selBtnGroup.find('span.selected').text(selectedRelease);
+            // Deactivate dropdown button
+            selBtnGroup.find('.dropdown-toggle').attr('disabled', true);
+            // Activate waiting status
+            let upgradeStatus = selBtnGroup.find('span.upgrade-status');
+            upgradeStatus.find('.status-none').addClass('d-none');
+            upgradeStatus.find('.status-waiting').removeClass('d-none');
+            // Activate cancel button
+            selBtnGroup.siblings('.btn-cancel-update')
+              .addClass('btn-danger').attr('disabled', false);
+          });
+        }
       });
     }
   });
@@ -58,38 +75,54 @@ let cancelAllDeviceUpdates = function(event) {
   let row = $(event.target).closest('tr');
   let rows = row.siblings();
   let idsObj = {};
-  // Make sure to fallback to default text
-  $('#all-devices').find('span.selected').text('Escolher');
 
-  rows.each(function(idx) {
-    let deviceModel = $(this).data('deviceModel');
-    if (deviceModel) {
-      if ($(this).prop('id') !== undefined && $(this).prop('id').length > 0) {
-        let id = $(this).prop('id');
-        let rel = $(this).data('device-release');
-        idsObj[id] = rel;
-      }
-    }
-  });
+  swal({
+    type: 'warning',
+    title: 'Atenção!',
+    text: 'Isso cancelará todas as atualizações de firmware em ' +
+    'progresso nesta página.',
+    confirmButtonText: 'Prosseguir',
+    confirmButtonColor: '#4db6ac',
+    cancelButtonText: 'Cancelar',
+    cancelButtonColor: '#f2ab63',
+    showCancelButton: true,
+  }).then(function(result) {
+    if (result.value) {
+      // Make sure to fallback to default text
+      $('#all-devices').find('span.selected').text('Escolher');
 
-  $.post('/devicelist/updateall',
-         {content: JSON.stringify({ids: idsObj, do_update: false})})
-  .done(function(res) {
-    if (res.success) {
-      res.devices.forEach(function(deviceId) {
-        let selBtnGroup = $('#' + $.escapeSelector(deviceId))
-          .find('.btn-group .btn-group');
-        let selBtnCancel = $('#' + $.escapeSelector(deviceId))
-          .find('.btn-group .btn-cancel-update');
-        selBtnGroup.find('span.selected').text('Escolher');
-        // Activate dropdown button
-        selBtnGroup.find('.dropdown-toggle').attr('disabled', false);
-        // Deactivate waiting status
-        let upgradeStatus = selBtnGroup.find('span.upgrade-status');
-        upgradeStatus.find('.status-none').removeClass('d-none');
-        upgradeStatus.find('.status-waiting').addClass('d-none');
-        // Deactivate cancel button
-        selBtnCancel.removeClass('btn-danger').attr('disabled', true);
+      rows.each(function(idx) {
+        let deviceModel = $(this).data('deviceModel');
+        if (deviceModel) {
+          if ($(this).prop('id') !== undefined &&
+              $(this).prop('id').length > 0) {
+            let id = $(this).prop('id');
+            let rel = $(this).data('device-release');
+            idsObj[id] = rel;
+          }
+        }
+      });
+
+      $.post('/devicelist/updateall',
+             {content: JSON.stringify({ids: idsObj, do_update: false})})
+      .done(function(res) {
+        if (res.success) {
+          res.devices.forEach(function(deviceId) {
+            let selBtnGroup = $('#' + $.escapeSelector(deviceId))
+              .find('.btn-group .btn-group');
+            let selBtnCancel = $('#' + $.escapeSelector(deviceId))
+              .find('.btn-group .btn-cancel-update');
+            selBtnGroup.find('span.selected').text('Escolher');
+            // Activate dropdown button
+            selBtnGroup.find('.dropdown-toggle').attr('disabled', false);
+            // Deactivate waiting status
+            let upgradeStatus = selBtnGroup.find('span.upgrade-status');
+            upgradeStatus.find('.status-none').removeClass('d-none');
+            upgradeStatus.find('.status-waiting').addClass('d-none');
+            // Deactivate cancel button
+            selBtnCancel.removeClass('btn-danger').attr('disabled', true);
+          });
+        }
       });
     }
   });
@@ -98,33 +131,49 @@ let cancelAllDeviceUpdates = function(event) {
 let updateDevice = function(event) {
   let selRelease = $(this).text();
   let selBtnGroup = $(event.target).closest('.btn-group');
-  // Change current selected release
-  selBtnGroup.find('span.selected').text(selRelease);
-  // Disable selection
-  let dropdownBtn = selBtnGroup.find('.dropdown-toggle');
-  dropdownBtn.attr('disabled', true);
-  // Submit update
-  let row = $(event.target).closest('tr');
-  let id = row.prop('id');
-  $.ajax({
-    url: '/devicelist/update/' + id + '/' + selRelease,
-    type: 'post',
-    traditional: true,
-    data: {do_update: true},
-    success: function(res) {
-      if (res.success) {
-        // Activate waiting status
-        let upgradeStatus = selBtnGroup.find('span.upgrade-status');
-        upgradeStatus.find('.status-none').addClass('d-none');
-        upgradeStatus.find('.status-waiting').removeClass('d-none');
-        // Activate cancel button
-        selBtnGroup.siblings('.btn-cancel-update')
-          .addClass('btn-danger').attr('disabled', false);
-      }
-    },
-    error: function(xhr, status, error) {
-      dropdownBtn.attr('disabled', false);
-    },
+
+  swal({
+    type: 'warning',
+    title: 'Atenção!',
+    text: 'A atualização de firmware dura aproximadamente 3 minutos. ' +
+    'O roteador não deverá ser desligado durante esse procedimento. ' +
+    'Comunique seu usuário antes de realizar esse procedimento.',
+    confirmButtonText: 'Prosseguir',
+    confirmButtonColor: '#4db6ac',
+    cancelButtonText: 'Cancelar',
+    cancelButtonColor: '#f2ab63',
+    showCancelButton: true,
+  }).then(function(result) {
+    if (result.value) {
+      // Change current selected release
+      selBtnGroup.find('span.selected').text(selRelease);
+      // Disable selection
+      let dropdownBtn = selBtnGroup.find('.dropdown-toggle');
+      dropdownBtn.attr('disabled', true);
+      // Submit update
+      let row = $(event.target).closest('tr');
+      let id = row.prop('id');
+      $.ajax({
+        url: '/devicelist/update/' + id + '/' + selRelease,
+        type: 'post',
+        traditional: true,
+        data: {do_update: true},
+        success: function(res) {
+          if (res.success) {
+            // Activate waiting status
+            let upgradeStatus = selBtnGroup.find('span.upgrade-status');
+            upgradeStatus.find('.status-none').addClass('d-none');
+            upgradeStatus.find('.status-waiting').removeClass('d-none');
+            // Activate cancel button
+            selBtnGroup.siblings('.btn-cancel-update')
+              .addClass('btn-danger').attr('disabled', false);
+          }
+        },
+        error: function(xhr, status, error) {
+          dropdownBtn.attr('disabled', false);
+        },
+      });
+    }
   });
 };
 
