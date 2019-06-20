6540cc9e1a12b1eac85ba0bf897d704ea49fe2d9
Guilherme Senges <guilherme@anlix.io>
2019-06-20 13:01:29 -0300

    Added remaining api entries from user routes. Marked API v1 URLs as deprecated

--- routes/api/v2.js
+++ routes/api/v2.js
@@ -1,66 +1,71 @@
 
 const express = require('express');
 const deviceListController = require('../../controllers/device_list');
+const userController = require('../../controllers/user');
 const authController = require('../../controllers/auth');
 
 let router = express.Router();
 
+// ***************
+// *** Devices ***
+// ***************
+
 // Query devices
-router.route('/search').put(
+router.route('/device/search').put(
   authController.ensureAPIAccess,
   authController.ensurePermission('grantAPIAccess'),
   deviceListController.searchDeviceReg);
 
 // Change device update/upgrade status
-router.route('/update/:id/:release').put(
+router.route('/device/update/:id/:release').put(
   authController.ensureAPIAccess,
   authController.ensurePermission('grantAPIAccess'),
   deviceListController.changeUpdate);
 
 // Delete device
-router.route('/delete/:id').delete(
+router.route('/device/delete/:id').delete(
   authController.ensureAPIAccess,
   authController.ensurePermission('grantAPIAccess'),
   deviceListController.delDeviceReg);
 
 // Get device registry
-router.route('/update/:id').get(
+router.route('/device/update/:id').get(
   authController.ensureAPIAccess,
   authController.ensurePermission('grantAPIAccess'),
   deviceListController.getDeviceReg);
 
 // Change device registry
-router.route('/update/:id').put(
+router.route('/device/update/:id').put(
   authController.ensureAPIAccess,
   authController.ensurePermission('grantAPIAccess'),
   deviceListController.setDeviceReg);
 
 // Create device registry
-router.route('/create').put(
+router.route('/device/create').put(
   authController.ensureAPIAccess,
   authController.ensurePermission('grantAPIAccess'),
   deviceListController.createDeviceReg);
 
 // Get first boot logs
-router.route('/firstlog/:id').get(
+router.route('/device/firstlog/:id').get(
   authController.ensureAPIAccess,
   authController.ensurePermission('grantAPIAccess'),
   deviceListController.getFirstBootLog);
 
 // GET last boot logs
-router.route('/lastlog/:id').get(
+router.route('/device/lastlog/:id').get(
   authController.ensureAPIAccess,
   authController.ensurePermission('grantAPIAccess'),
   deviceListController.getLastBootLog);
 
 // Send a message using MQTT
-router.route('/command/:id/:msg').put(
+router.route('/device/command/:id/:msg').put(
   authController.ensureAPIAccess,
   authController.ensurePermission('grantAPIAccess'),
   deviceListController.sendMqttMsg);
 
 // Set/Get Port forward
-router.route('/portforward/:id').get(
+router.route('/device/portforward/:id').get(
   authController.ensureAPIAccess,
   authController.ensurePermission('grantAPIAccess'),
   deviceListController.getPortForward)
@@ -70,9 +75,61 @@ router.route('/portforward/:id').get(
   deviceListController.setPortForward);
 
 // Set/Get Ping hosts list
-router.route('/pinghostslist/:id').put(
+router.route('/device/pinghostslist/:id').get(
+  authController.ensureAPIAccess,
+  authController.ensurePermission('grantAPIAccess'),
+  deviceListController.getPingHostsList)
+                                  .put(
   authController.ensureAPIAccess,
   authController.ensurePermission('grantAPIAccess'),
   deviceListController.setPingHostsList);
+
+// *************
+// *** Users ***
+// *************
+
+router.route('/user/get/all').get(
+  authController.ensureAPIAccess,
+  authController.ensurePermission('grantAPIAccess'),
+  userController.getUsers);
+
+router.route('/user/edit/:id').put(
+  authController.ensureAPIAccess,
+  authController.ensurePermission('grantAPIAccess'),
+  userController.editUser);
+
+router.route('/user/new').put(
+  authController.ensureAPIAccess,
+  authController.ensurePermission('grantAPIAccess'),
+  userController.postUser);
+
+router.route('/user/del').put(
+  authController.ensureAPIAccess,
+  authController.ensurePermission('grantAPIAccess'),
+  userController.deleteUser);
+
+// *************
+// *** Roles ***
+// *************
+
+router.route('/role/get/all').get(
+  authController.ensureAPIAccess,
+  authController.ensurePermission('grantAPIAccess'),
+  userController.getRoles);
+
+router.route('/role/new').put(
+  authController.ensureAPIAccess,
+  authController.ensurePermission('grantAPIAccess'),
+  userController.postRole);
+
+router.route('/role/edit/:id').put(
+  authController.ensureAPIAccess,
+  authController.ensurePermission('grantAPIAccess'),
+  userController.editRole);
+
+router.route('/role/del').put(
+  authController.ensureAPIAccess,
+  authController.ensurePermission('grantAPIAccess'),
+  userController.deleteRole);
 
 module.exports = router;
