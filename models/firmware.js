97fd4f52bcb50a511cc35e23f483948bc9149dd2
Guilherme Senges <guilherme@anlix.io>
2019-06-24 12:02:00 -0300

    Applied JS Linter on file

--- models/firmware.js
+++ models/firmware.js
@@ -1,18 +1,19 @@
 
-var mongoose = require('mongoose');
-var mongoosePaginate = require('mongoose-paginate');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
 
-var firmwareSchema = new Schema({
-  vendor : {type: String, required: true},
-  model : {type: String, required: true},
-  version : {type: String, required: true},
-  release : {type: String, required: true},
-  filename : {type: String, required: true}
+let Schema = mongoose.Schema;
+
+let firmwareSchema = new Schema({
+  vendor: {type: String, required: true},
+  model: {type: String, required: true},
+  version: {type: String, required: true},
+  release: {type: String, required: true},
+  filename: {type: String, required: true},
 });
 
 firmwareSchema.plugin(mongoosePaginate);
 
-var Firmware = mongoose.model('Firmware', firmwareSchema );
+let Firmware = mongoose.model('Firmware', firmwareSchema);
 
 module.exports = Firmware;
