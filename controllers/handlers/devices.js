22ec1a5c7cd40291dcf4b5fa02086d443f224ffd
Guilherme Senges <guilherme@anlix.io>
2020-05-06 13:21:23 -0300

    Improved date difference calculation and added tests

--- controllers/handlers/devices.js
+++ controllers/handlers/devices.js
@@ -1,17 +1,36 @@
 
 let deviceHandlers = {};
 
+deviceHandlers.diffDateUntilNowInSeconds = function(pastDate) {
+  let pastDateParsed = NaN;
+  const dateObjType = Object.prototype.toString.call(pastDate);
+
+  if (dateObjType === '[object Date]') {
+    // Remove any timezone representation and convert to UTC
+    pastDateParsed = pastDate.toISOString();
+    pastDateParsed = Date.parse(pastDateParsed);
+  } else if (dateObjType === '[object String]') {
+    // Always UTC conversion
+    pastDateParsed = Date.parse(pastDate);
+  }
+  // Assume old date if can't be parsed
+  if (isNaN(pastDateParsed)) {
+    pastDateParsed = new Date(1970, 1, 1);
+  }
+  let justNow = Date.now();
+  let devTimeDiff = Math.abs(justNow - pastDateParsed);
+  let devTimeDiffSeconds = Math.floor(devTimeDiff / 1000);
+
+  return devTimeDiffSeconds;
+};
+
 deviceHandlers.isOnline = function(dateLastSeen) {
   let isOnline = false;
   let offlineThresh = 3; // seconds
 
-  let lastSeen = ((dateLastSeen) ?
-                  Date.parse(dateLastSeen) : new Date(1970, 1, 1));
-  let justNow = Date.now();
-  let devTimeDiff = Math.abs(justNow - lastSeen);
-  let devTimeDiffSeconds = Math.floor(devTimeDiff / 1000);
+  const diffInSeconds = deviceHandlers.diffDateUntilNowInSeconds(dateLastSeen);
 
-  if (devTimeDiffSeconds <= offlineThresh) {
+  if (diffInSeconds <= offlineThresh) {
     isOnline = true;
   } else {
     isOnline = false;
@@ -21,14 +40,11 @@ deviceHandlers.isOnline = function(dateLastSeen) {
 
 deviceHandlers.isTooOld = function(dateLastSeen) {
   let isOld = false;
-  let lastSeen = ((dateLastSeen) ?
-                  Date.parse(dateLastSeen) : new Date(1970, 1, 1));
-  let justNow = Date.now();
-  let devTimeDiff = Math.abs(justNow - lastSeen);
-  let devTimeDiffSeconds = Math.floor(devTimeDiff / 1000);
+
+  const diffInSeconds = deviceHandlers.diffDateUntilNowInSeconds(dateLastSeen);
 
   // 24 hours
-  if (devTimeDiffSeconds >= 86400) {
+  if (diffInSeconds >= 86400) {
     isOld = true;
   }
   return isOld;
--- test/unit/handlers.unit.test.js
+++ test/unit/handlers.unit.test.js
@@ -3,13 +3,60 @@
 const deviceHandlers = require('../../controllers/handlers/devices');
 
 describe('Handlers', () => {
-  test('Device must return online status', () => {
-    // Return 'now' date
-    let testDateOnline = new Date();
-    testDateOnline = testDateOnline.toISOString();
+  test('Date difference from 2 seconds until now must be 2 seconds', () => {
+    // Return now - 2 seconds
+    let testDate = new Date(Date.now() - 2000);
     // Test
-    let isOnlineReturn = deviceHandlers.isOnline(testDateOnline);
+    let seconds = deviceHandlers.diffDateUntilNowInSeconds(testDate);
+    expect(seconds).toEqual(2);
+  });
+
+  test('Date str difference from 2 seconds until now must be 2 seconds', () => {
+    // Return now - 2 seconds
+    let testDate = new Date(Date.now() - 2000);
+    testDate = testDate.toISOString();
+    // Test
+    let seconds = deviceHandlers.diffDateUntilNowInSeconds(testDate);
+    expect(seconds).toEqual(2);
+  });
+
+  test('Date difference from invalid until now must be epoch', () => {
+    // Return now - 2 seconds
+    let testDate = 'not-a-date';
+    // Test
+    let seconds = deviceHandlers.diffDateUntilNowInSeconds(testDate);
+    expect(seconds).toBeGreaterThan(500000);
+  });
+
+  test('Device must return online status when date is ISO string', () => {
+    // Return 'now' date
+    let testDate = new Date();
+    testDate = testDate.toISOString();
+    // Test
+    let isOnlineReturn = deviceHandlers.isOnline(testDate);
     expect(isOnlineReturn).toEqual(true);
+  });
+
+  test('Device must return online status when date is Date', () => {
+    // Return 'now' date
+    let testDate = new Date();
+    // Test
+    let isOnlineReturn = deviceHandlers.isOnline(testDate);
+    expect(isOnlineReturn).toEqual(true);
+  });
+
+  test('Device must return offline status when date is invalid', () => {
+    let testDate = 'not-a-date';
+    // Test
+    let isOnlineReturn = deviceHandlers.isOnline(testDate);
+    expect(isOnlineReturn).toEqual(false);
+  });
+
+  test('Device must return offline status when date is old', () => {
+    let testDate = new Date(1970, 1, 1);
+    // Test
+    let isOnlineReturn = deviceHandlers.isOnline(testDate);
+    expect(isOnlineReturn).toEqual(false);
   });
 });
 
