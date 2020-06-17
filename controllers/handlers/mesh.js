bc43c31c70a67cb81f684ccdfd311085b51924c8
Thurler <gt@anlix.io>
2020-06-17 00:14:59 -0300

    Expanded mesh handlers to propagate a firmware update to all the network

--- controllers/handlers/mesh.js
+++ controllers/handlers/mesh.js
@@ -1,5 +1,8 @@
 const DeviceModel = require('../../models/device');
+const messaging = require('../messaging');
 const mqtt = require('../../mqtts');
+const updateScheduler = require('../update_scheduler');
+const deviceHandlers = require('./devices');
 
 let meshHandlers = {};
 
@@ -36,8 +39,7 @@ meshHandlers.syncSlaves = function(master, slaveReferences=null) {
       if (err) {
         console.log('Attempt to modify mesh slave '+ slaveMac +' from master ' +
           master._id + ' failed: cant get slave device.');
-      } else 
-      if (!slaveDevice) {
+      } else if (!slaveDevice) {
         console.log('Attempt to modify mesh slave '+ slaveMac +' from master ' +
           master._id + ' failed: cant get slave device.');
       } else {
@@ -91,6 +93,92 @@ meshHandlers.enhanceSearchResult = async function(result) {
     }
   }
   return extraResults;
+}
+
+meshHandlers.propagateUpdate = async function(targetMac, release) {
+  DeviceModel.findById(targetMac, function(err, device) {
+    if (err) {
+      console.log('Attempt to access mesh slave '+ targetMac +
+                  ' failed: database error.');
+    } else if (!device) {
+      console.log('Attempt to access mesh slave '+ targetMac +
+                  ' failed: device not found.');
+    } else {
+      device.do_update = true;
+      device.do_update_status = 0; // waiting
+      device.release = release;
+      messaging.sendUpdateMessage(device);
+      device.save(function(err) {
+        if (!err) {
+          mqtt.anlixMessageRouterUpdate(targetMac);
+          // Start ack timeout
+          deviceHandlers.timeoutUpdateAck(targetMac);
+        }
+      });
+    }
+  });
+}
+
+meshHandlers.updateMaster = async function(slaveMac, masterMac, release) {
+  DeviceModel.findById(masterMac, function(err, masterDevice) {
+    if (err) {
+      console.log('Attempt to access mesh master '+ masterMac +
+                  ' failed: database error.');
+    } else if (!masterDevice) {
+      console.log('Attempt to access mesh master '+ masterMac +
+                  ' failed: device not found.');
+    } else {
+      masterDevice.do_update_mesh_remaining -= 1;
+      masterDevice.save();
+      // Propagate update to next slave
+      let index = masterDevice.mesh_slaves.findIndex((s)=>s===slaveMac) + 1;
+      if (index > 0 && index < masterDevice.mesh_slaves.length) {
+        meshHandlers.propagateUpdate(masterDevice.mesh_slaves[index], release);
+      }
+    }
+  });
+}
+
+meshHandlers.syncUpdate = function(device, setQuery, release) {
+  // Only change information if device has slaves or a master
+  if ((!device.mesh_slaves || device.mesh_slaves.length === 0) &&
+      !device.mesh_master) {
+    return;
+  }
+  // If this is a master device, decrement remaining counter and propagate
+  // update to first slave in the list
+  if (device.mesh_slaves && device.mesh_slaves.length > 0) {
+    let current = device.do_update_mesh_remaining;
+    device.do_update_mesh_remaining = current - 1; // Used in device response
+    setQuery.do_update_mesh_remaining = current - 1;
+    meshHandlers.propagateUpdate(device.mesh_slaves[0], release);
+  }
+  // If this is a slave device, we simply update master information
+  if (device.mesh_master) {
+    meshHandlers.updateMaster(device._id, device.mesh_master, release);
+  }
+}
+
+meshHandlers.syncUpdateCancel = function(masterDevice, status=1) {
+  if (!masterDevice.mesh_slaves || masterDevice.mesh_slaves.length === 0) {
+    // Abort if not a mesh master - why would this be called?
+    return;
+  }
+  masterDevice.mesh_slaves.forEach((slaveMac)=>{
+    DeviceModel.findById(slaveMac, function(err, slaveDevice) {
+      if (err) {
+        console.log('Attempt to access mesh slave '+ slaveMac +
+                    ' failed: database error.');
+      } else if (!slaveDevice) {
+        console.log('Attempt to access mesh slave '+ slaveMac +
+                    ' failed: device not found.');
+      } else {
+        slaveDevice.do_update = false;
+        slaveDevice.do_update_status = status;
+        slaveDevice.save();
+      }
+    });
+  });
 }
 
 module.exports = meshHandlers;
