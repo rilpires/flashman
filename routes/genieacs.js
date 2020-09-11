84389abbe5767ee86ff9c1edff3e6725ac6bed6e
cassiohg <cassiohg@gmail.com>
2020-09-11 01:48:27 -0300

    added route entry to test request sessions.

--- routes/genieacs.js
+++ routes/genieacs.js
@@ -1,8 +1,11 @@
 const express = require('express');
 let genieDeviceInfoController = require('../controllers/acs_device_info');
+let genieCalls = require('../controllers/genie.js');
 
 let router = express.Router();
 
 router.route('/device/syn').post(genieDeviceInfoController.syncDevice);
+
+router.route('/test').post(genieCalls.test);
 
 module.exports = router;
