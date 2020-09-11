bb1edd429aa37d0e81f694e8470e8eb9c9dda342
cassiohg <cassiohg@gmail.com>
2020-09-11 01:55:59 -0300

    Revert "added route entry to test request sessions."
    This reverts commit 84389abbe5767ee86ff9c1edff3e6725ac6bed6e.

--- routes/genieacs.js
+++ routes/genieacs.js
@@ -1,11 +1,8 @@
 const express = require('express');
 let genieDeviceInfoController = require('../controllers/acs_device_info');
-let genieCalls = require('../controllers/genie.js');
 
 let router = express.Router();
 
 router.route('/device/syn').post(genieDeviceInfoController.syncDevice);
-
-router.route('/test').post(genieCalls.test);
 
 module.exports = router;
