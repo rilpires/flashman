56735801d63c4a997ba7abee348a8be4d0834836
cassiohg <cassiohg@gmail.com>
2021-02-03 17:06:26 -0300

    added semicolon to statements

--- controllers/handlers/util.js
+++ controllers/handlers/util.js
@@ -42,35 +42,36 @@ utilHandlers.deepCopyObject = function(obj) {
 
 const macRegex = /^([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})$/;
 const ipv4Regex = /^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}$/;
-const ipv6Regex = /^[0-9a-f]{1,4}(?::[0-9a-f]{1,4}){7}$|^(?:[0-9a-f]{1,4}:){6}(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}$/i
+const ipv6Regex = /^[0-9a-f]{1,4}(?::[0-9a-f]{1,4}){7}$|^(?:[0-9a-f]{1,4}:){6}(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}$/i;
 // const ipv6Regexp = /^[0-9a-f]{1,4}(?::[0-9a-f]{1,4}){7}$|^::(?:(?:[0-9a-f]{1,4}:){0,5}[0-9a-f]{1,4})?$|^(?:[0-9a-f]{1,4}:){1,6}:$|^(?:[0-9a-f]{1,4}:)+(?::[0-9a-f]{1,4})+$/i
 const domainNameRegex = /^[0-9a-z]+(?:-[0-9a-z]+)*(?:\.[0-9a-z]+(?:-[0-9a-z]+)*)+$/i;
 const testIPv6 = function (ipv6) {
-  if (ipv6.constructor !== String) return false
-  let parts = ipv6.split(":")
-  let maxparts = /:\d{1,3}\./.test(ipv6) ? 7 : 8 // has an ipv4 at the end or not.
-  if (parts.length > maxparts || parts.length < 3) return false
-  let hasDoubleColon = ipv6.indexOf("::") > -1
-  if (parts.length === maxparts && hasDoubleColon) return false
+  if (ipv6.constructor !== String) return false;
+  let parts = ipv6.split(":");
+  let maxparts = /:\d{1,3}\./.test(ipv6) ? 7 : 8; // has an ipv4 at the end or not.
+  if (parts.length > maxparts || parts.length < 3) return false;
+  let hasDoubleColon = ipv6.indexOf("::") > -1;
+  if (parts.length === maxparts && hasDoubleColon) return false;
   else if (hasDoubleColon) {
-    let notEmptyCounter = 0
+    let notEmptyCounter = 0;
     for (let i = 0; i < parts.length; i++)
-      if (parts[i].length > 0) notEmptyCounter++
-    let remaining = maxparts-notEmptyCounter
-    let substitute = ipv6[0] === ":" ? "" : ":"
+      if (parts[i].length > 0) notEmptyCounter++;
+    let remaining = maxparts-notEmptyCounter;
+    let substitute = ipv6[0] === ":" ? "" : ":";
     for (let i = 0; i < remaining; i++) substitute+="0:"
     if (ipv6[ipv6.length-1] === ":") substitute = substitute.slice(0, -1);
-    ipv6 = ipv6.replace("::", substitute)
+    ipv6 = ipv6.replace("::", substitute);
   }
-  return ipv6Regex.test(ipv6)
+  return ipv6Regex.test(ipv6);
 };
 
-utilHandlers.isFqdnValid = fqdn => fqdn.constructor === String &&
-  (fqdn === "" || domainNameRegex.test(fqdn) || ipv4Regex.test(fqdn) || testIPv6(fqdn))
+utilHandlers.isFqdnValid = (fqdn) => fqdn.constructor === String && (
+  fqdn === "" || domainNameRegex.test(fqdn) || ipv4Regex.test(fqdn) ||
+  testIPv6(fqdn));
 
-utilHandlers.isMacValid = mac => mac.constructor === String && macRegex.test(mac)
+utilHandlers.isMacValid = (mac) => mac.constructor === String && macRegex.test(mac);
 
-utilHandlers.isArrayObject = val => val instanceof Array ? true : false;
+utilHandlers.isArrayObject = (val) => val instanceof Array ? true : false;
 
 
 module.exports = utilHandlers;
