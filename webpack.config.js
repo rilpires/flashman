3ac55da735d6965247a04e8b53a1915c49058464
W1N3 <gabriel.silva.w1n3@gmail.com>
2021-02-04 19:57:23 -0300

    Full bundle of index.pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+// const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   target: 'web',
   mode: 'development',
-  externals: [nodeExternals()],
+  // externals: [nodeExternals()],
   entry: {
     index: ['./public/javascripts/device_validator.js',
     './public/javascripts/new_device.js',
@@ -19,6 +19,14 @@ module.exports = {
     './public/javascripts/show_upgrade_schedule_actions.js',
     './public/javascripts/update_device.js',
     './public/javascripts/table_anim.js',
+    './node_modules/jquery-highlight/jquery.highlight.js',
+    './node_modules/pako/dist/pako.js',
+    './node_modules/moment/min/moment.min.js',
+    './node_modules/tempusdominus-bootstrap-4/build/js/tempusdominus-bootstrap-4.min.js',
+    './node_modules/bs-stepper/dist/js/bs-stepper.min.js',
+    './node_modules/apexcharts/dist/apexcharts.min.js',
+    './node_modules/tags-input/tags-input.js',
+    './/node_modules/socket.io-client/dist/socket.io.js',
     ],
   },
   output: {
@@ -39,12 +47,12 @@ module.exports = {
       },*/
     ],
   },
-  resolve: {
+  /* resolve: {
     fallback: {
       'util': require.resolve('util/'),
       'path-browserify': require.resolve('path-browserify/'),
       'stream-browserify': require.resolve('stream-browserify/'),
       'assert': require.resolve('assert/'),
     },
-  },
+  },*/
 };
