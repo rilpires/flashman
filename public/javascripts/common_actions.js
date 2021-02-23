7c1072744bc7f5d96faac5251ec38995d4299a21
Guilherme Senges <guilherme@anlix.io>
2021-02-23 11:31:46 -0300

    Added basic dynamic notification logic in frontend.

--- public/javascripts/common_actions.js
+++ public/javascripts/common_actions.js
@@ -1,7 +1,7 @@
+
 const displayAlertMsg = function(res) {
   $('#frame-modal-alert .modal-dialog').removeClass(
-    'modal-success modal-danger'
-  );
+    'modal-success modal-danger');
   $('#frame-modal-alert .modal-dialog').addClass('modal-' + res.type);
   $('#frame-modal-alert .alert-message').html(res.message);
   $('#frame-modal-alert').modal('show');
@@ -14,3 +14,35 @@ const secondsTimeSpanToHMS = function(s) {
   s -= m * 60;
   return h + ':' + (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s);
 };
+
+let fetchNotifications = function() {
+  $.ajax({type: 'POST',
+          url: '/notification/fetch',
+          traditional: true,
+          data: {targets: ['general']}})
+  .done((response) => {
+    let alertsBody = $('#alerts-flashman-body');
+    if (response.notifications.length > 0) {
+      for (let notification of response.notifications) {
+        alertsBody.append(
+          $('<div>').addClass('row mb-3').append(
+            $('<div>').addClass('card w-100').append(
+              $('<div>').addClass('card-body text-center').append(
+                $('<p>').text(notification.message),
+              ),
+            ),
+          ),
+        );
+      }
+      $('.bell-notification').addClass('swing infinite');
+      $('.bell-counter').attr('count', response.notifications.length);
+      $('.bell-counter').show();
+    }
+  });
+};
+
+$(document).ready(function() {
+  if (!window.location.href.includes('/login')) {
+    fetchNotifications();
+  }
+});
