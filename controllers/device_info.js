a60e0138ebf6f28efdbaf87862ffb1aa0060b46f
cassiohg <cassiohg@gmail.com>
2021-03-30 09:06:07 -0300

    Using temporary variables with short names to make code shorter in order to fit more code into 80 character line limit and therefore use fewer lines

--- controllers/device_info.js
+++ controllers/device_info.js
@@ -715,20 +715,14 @@ deviceInfoController.updateDevicesInfo = function(req, res) {
           // combining 'Device' and 'Config'.
           // if data_collecting exists in Config.
           if (matchedDevice.data_collecting !== undefined) {
-            if (matchedDevice.data_collecting.is_active !== undefined) {
-              // matchedDevice value && matchedConfig value.
-              data_collecting.is_active = data_collecting.is_active &&
-                matchedDevice.data_collecting.is_active;
-            }
-            if (matchedDevice.data_collecting.has_latency !== undefined) {
-              // matchedDevice value && matchedConfig value.
-              data_collecting.has_latency = data_collecting.has_latency &&
-                matchedDevice.data_collecting.has_latency;
-            }
-            if (matchedDevice.data_collecting.ping_fqdn !== undefined) {
-              // preference for matchedDevice value.
-              data_collecting.ping_fqdn = matchedDevice.data_collecting.ping_fqdn;
-            }
+            let d = matchedDevice.data_collecting // parameters from device model.
+            let p = data_collecting // the final parameters.
+            // matchedDevice value && matchedConfig value if it exists.
+            d.is_active !== undefined ? p.is_active = p.is_active && d.is_active : _;
+            // matchedDevice value && matchedConfig value if it exists.
+            d.has_latency !== undefined ? p.has_latency = p.has_latency && d.has_latency : _;
+            // preference for matchedDevice value if it exists.
+            d.ping_fqdn !== undefined ? p.ping_fqdn = d.ping_fqdn : _;
           }
 
           const isDevOn = Object.values(mqtt.unifiedClientsMap).some((map)=>{
