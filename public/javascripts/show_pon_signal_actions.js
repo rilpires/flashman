e5093a353fa7959d2a870877bccb72717a31cd5a
JotaPe <jotape@protonmail.com>
2021-04-22 22:31:17 -0300

    add differente functions to deal using get http

--- public/javascripts/show_pon_signal_actions.js
+++ public/javascripts/show_pon_signal_actions.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   let ponSignalRXId = '';
   let ponSignalTXId = '';
+  let ponSignalMeasure;
 
   const refreshPonSignal = function(deviceId) {
     $('#btn-pon-signal-refresh').prop('disabled', true);
@@ -10,6 +11,7 @@ $(document).ready(function() {
       dataType: 'json',
       success: function(res) {
         if (res.success) {
+          ponSignalMeasure = res.ponsignalmeasure;
           $('#btn-pon-signal-refresh > i').addClass('animated rotateOut infinite');
           if ($('#pon-signal-graphs').is(':hidden')) {
             $('#pon-signal-placeholder-ready').hide();
@@ -37,6 +39,7 @@ $(document).ready(function() {
   };
 
   // Important: include and initialize socket.io first using socket var
+  /*
   socket.on('PONSIGNAL', function(macaddr, data) {
     console.log(data);
     if (data.ponsignalmeasure && macaddr === $('#pon-signal-hlabel').text()) {
@@ -96,10 +99,71 @@ $(document).ready(function() {
       $('#pon-signal-graphs').show();
     }
   });
+  */
+
+  const createPonSignalTable = function(ponSignalMeasure, macaddr) {
+    if (ponSignalMeasure && macaddr === $('#pon-signal-hlabel').text()) {
+      $('#pon-signal-graph').empty();
+      let rxMeasure = [];
+      let txMeasure = Object.keys(ponSignalMeasure).map(function(time) {
+        let epochInUs = Number(time) * 1000;
+        // Also create upBytes array
+        rxMeasure.push([epochInUs, ponSignalMeasure[time][1]]);
+        // Downstream
+        return [epochInUs, ponSignalMeasure[time][0]];
+      });
+      let rxOptions = {
+        chart: {id: 'downChart', type: 'line', toolbar: false,
+                animations: {enabled: false}},
+        tooltip: {x: {format: 'HH:mm'}},
+        theme: {palette: 'palette4'},
+        title: {text: 'Download', align: 'center'},
+        series: [{name: 'Download', data: rxMeasure}],
+        xaxis: {type: 'datetime', labels: {datetimeUTC: false}},
+      };
+      let txOptions = {
+        chart: {id: 'upChart', type: 'line', toolbar: false,
+                animations: {enabled: false}},
+        tooltip: {x: {format: 'HH:mm'}},
+        theme: {palette: 'palette5'},
+        title: {text: 'Upload', align: 'center'},
+        series: [{name: 'Upload', data: txMeasure}],
+        xaxis: {type: 'datetime', labels: {datetimeUTC: false}},
+      };
+      if (ponSignalRXId === '') {
+        let chartDownObj = new ApexCharts(
+          document.querySelector('#pon-signal-down-graph'),
+          rxOptions,
+        );
+        ponSignalRXId = rxOptions.chart.id;
+        chartDownObj.render();
+      } else {
+        ApexCharts.exec(ponSignalRXId, 'updateOptions', rxOptions, false, true);
+      }
+      if (ponSignalTXId === '') {
+        let chartTXObj = new ApexCharts(
+          document.querySelector('#pon-signal-up-graph'),
+          txOptions,
+        );
+        ponSignalTXId = txOptions.chart.id;
+        chartTXObj.render();
+      } else {
+        ApexCharts.exec(ponSignalTXId, 'updateOptions', txOptions, false, true);
+      }
+      // Adjust modal content
+      $('#btn-pon-signal-refresh').prop('disabled', false);
+      $('#btn-pon-signal-refresh > i').removeClass('animated rotateOut infinite');
+      $('#pon-signal-placeholder-ready').hide();
+      $('#pon-signal-placeholder-progress').hide();
+      $('#pon-signal-placeholder-none').hide();
+      $('#pon-signal-graphs').show();
+    }
+  }
 
   $(document).on('click', '#btn-pon-signal-refresh', function(event) {
     let id = $('#pon-signal-hlabel').text();
     refreshPonSignal(id);
+    createPonSignalTable(ponSignalMeasure, id)
   });
 
   $(document).on('click', '.btn-pon-signal-modal', function(event) {
