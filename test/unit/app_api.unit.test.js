f9b721118337daa2db2f12a330056bc5500fb4dd
JotaPe <jotape@protonmail.com>
2021-06-24 21:23:19 -0300

    add mongodb jest stuff and using it on tests

--- test/unit/app_api.unit.test.js
+++ test/unit/app_api.unit.test.js
@@ -1,37 +1,67 @@
+const mongoose = require('mongoose');
 const appDeviceAPIController = require('../../controllers/app_device_api');
+const DeviceModel = require('../../models/device');
+const DeviceVersionModel = require('../../models/device_version')
 
 describe('App API', () => {
   let modelZteF670L = 'F670L';
 
+  beforeAll(async () => {
+    await mongoose.connect(
+      "mongodb://" + process.env.FLM_MONGODB_HOST + ":27017/flashman",
+      {
+        useNewUrlParser: true,
+        serverSelectionTimeoutMS: 2 ** 31 - 1, // biggest positive signed integer with 32 bits.
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      },
+      (err) => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+      }
+    );
+  });
+
   test('check feature not tr069', () => {
-    expect(appDeviceAPIController.checkFeature('NOTTR069', 'wps', false));
+    let device = new DeviceModel({ model: 'NOTTR069'});
+    console.log(deviceVersion);
+    expect(appDeviceAPIController.checkFeature(device.model, 'wps', device.use_tr069));
   });
 
   test('check feature wps F670L', () => {
-    expect(appDeviceAPIController.checkFeature(modelZteF670L, 'wps', true));
+    let device = new DeviceModel({ use_tr069: true, model: 'F670L'});
+    expect(appDeviceAPIController.checkFeature(device.model, 'wps', device.use_tr069));
   });
 
   test('check feature upnp F670L', () => {
+    let device = new DeviceModel({ use_tr069: true, model: 'F670L'});
     expect(
-      appDeviceAPIController.checkFeature(modelZteF670L, 'upnp', true)
+      appDeviceAPIController.checkFeature(device.model, 'upnp', device.use_tr069),
     ).toBe(false);
   });
 
   test('check feature speedTest F670L', () => {
+    let device = new DeviceModel({ use_tr069: true, model: 'F670L'});
     expect(
-      appDeviceAPIController.checkFeature(modelZteF670L, 'speedTest', true)
+      appDeviceAPIController.checkFeature(device.model, 'speedTest', device.use_tr069),
     ).toBe(false);
   });
 
   test('check feature speedTestLimit F670L', () => {
+    let device = new DeviceModel({ use_tr069: true, model: 'F670L'});
     expect(
-      appDeviceAPIController.checkFeature(modelZteF670L, 'speedTestLimit', true)
+      appDeviceAPIController.checkFeature(device.model, 'speedTestLimit', device.use_tr069),
     ).toBe(false);
   });
 
   test('check feature blockDevices F670L', () => {
+    let device = new DeviceModel({ use_tr069: true, model: 'F670L'});
     expect(
-      appDeviceAPIController.checkFeature(modelZteF670L, 'blockDevices', true)
+      appDeviceAPIController.checkFeature(device.model, 'blockDevices', device.use_tr069),
     ).toBe(false);
   });
+
 });
