4ab2339490fec8468f181aba12ea112c8e3f7127
W1N3 <gabriel.silva.w1n3@gmail.com>
2021-07-08 17:26:42 -0300

    firmware model increases

--- models/firmware.js
+++ models/firmware.js
@@ -6,6 +6,7 @@ let Schema = mongoose.Schema;
 
 let firmwareSchema = new Schema({
   vendor: {type: String, required: true},
+  // productClass == model
   model: {type: String, required: true},
   version: {type: String, required: true},
   release: {type: String, required: true},
@@ -14,6 +15,12 @@ let firmwareSchema = new Schema({
   filename: {type: String, required: true},
   is_beta: {type: Boolean, default: false},
   is_restricted: {type: Boolean, default: false},
+  cpe_type: {
+    type: String,
+    enum: ['flashbox', 'tr069'],
+    default: 'flashbox',
+  }, // needs migration
+  oui: {type: String},
 });
 
 firmwareSchema.plugin(mongoosePaginate);
