1468cd26615305aac8685c52efb8afee0836b023
Thurler <gt@anlix.io>
2021-07-12 16:15:45 -0300

    Updated tests with multiple models and flashbox version

--- test/unit/device_version.unit.test.js
+++ test/unit/device_version.unit.test.js
@@ -1,89 +1,24 @@
 const mongoose = require('mongoose');
-const DeviceModel = require('../../models/device');
 const DeviceVersion = require('../../models/device_version');
 
-describe('DeviceVersion API', () => {
-  let zteF670LData = {
-    _id: '0C:F0:B4:46:1F:CE',
-    measure_config: {is_active: false},
-    use_tr069: true,
-    version: 'V1.2.3',
-    wifi_state: 0,
-    wifi_hidden: 0,
-    wifi_power: 100,
-    wifi_is_5ghz_capable: true,
-    wifi_state_5ghz: 0,
-    wifi_hidden_5ghz: 0,
-    wifi_power_5ghz: 100,
-    upnp_requests: [],
-    mesh_mode: 0,
-    mesh_slaves: [],
-    bridge_mode_enabled: false,
-    bridge_mode_switch_disable: true,
-    ipv6_enabled: 2,
-    do_update_status: 1,
-    do_update_mesh_remaining: 0,
-    mqtt_secret_bypass: false,
-    ping_hosts: [
-      'www.google.com',
-      'www.youtube.com',
-      'www.facebook.com',
-      'www.instagram.com',
-    ],
-    sys_up_time: 631109,
-    wan_up_time: 631063,
-    latitude: 0,
-    longitude: 0,
-    wps_is_active: false,
-    wps_last_connected_mac: '',
-    serial_tr069: 'MKPGB4461FCE',
-    acs_id: '0CF0B4-GONUAC001-MKPGB4461FCE',
-    model: 'GONUAC001',
-    installed_release: 'V1.2.3',
-    release: '0000-ONU',
-    connection_type: 'pppoe',
-    pppoe_user: 'admin123',
-    pppoe_password: 'admin123',
-    wifi_ssid: 'STAVIX_2.4G_Anlix',
-    wifi_channel: '11',
-    wifi_mode: '11n',
-    wifi_ssid_5ghz: 'STAVIX_5G_Anlix',
-    wifi_channel_5ghz: '44',
-    wifi_mode_5ghz: '11ac',
-    lan_subnet: '192.168.111.1',
-    lan_netmask: 24,
-    ip: '192.168.89.250',
-    wan_ip: '192.168.89.250',
-    wan_negociated_speed: '0',
-    wan_negociated_duplex: 'Half',
-    created_at: {$date: '2021-04-05T17:24:15.174Z'},
-    last_contact: {$date: '2021-06-15T20:10:09.061Z'},
-    lan_devices: [],
-    ap_survey: [],
-    mesh_routers: [],
-    apps: [],
-    speedtest_results: [],
-    __v: 1,
-    is_license_active: true,
-    wifi_password: 'landufrj123',
-    wifi_password_5ghz: 'landufrj123',
-    do_update_parameters: true,
-    external_reference: {data: 'Stavix Greatek', kind: 'Outro'},
-    wifi_band: 'auto',
-    wifi_band_5ghz: 'auto',
-    mesh_id: 'D6q91LT9-7xdYg',
-    mesh_key: 'eEITLqC5A0EYIKVbRoIIV8TkmGU',
-    pon_status: 'Up',
-    pon_txpower: 2,
-    pon_rxpower: -13,
-    port_forward_rules: [],
-    vlan: [],
-    acs_sync_loops: 0,
-  };
+const noRangePortForwawrdOpts = {
+ simpleSymmetric: true,
+ simpleAsymmetric: true,
+ rangeSymmetric: false,
+ rangeAsymmetric: false,
+};
 
-  beforeAll(async () => {
+const noAsymRangePortForwawrdOpts = {
+ simpleSymmetric: true,
+ simpleAsymmetric: true,
+ rangeSymmetric: true,
+ rangeAsymmetric: false,
+};
+
+describe('DeviceVersion API', () => {
+  beforeAll(async (done) => {
     await mongoose.connect(
-      'mongodb://' + process.env.FLM_MONGODB_HOST + ':27017/flashman',
+      'mongodb://' + process.env.FLM_MONGODB_HOST + ':27017/flashman-tests',
       {
         useNewUrlParser: true,
         serverSelectionTimeoutMS: 2 ** 31 - 1,
@@ -98,116 +33,146 @@ describe('DeviceVersion API', () => {
         }
       },
     );
+    done();
   });
 
-  test('check feature not tr069', () => {
-    let device = new DeviceModel({model: 'NOTTR069'});
-    expect(DeviceVersion.checkFeature(device.model, 'wps'));
+  afterAll(async (done) => {
+    await mongoose.connection.close();
+    done();
   });
 
-  test('check feature wps F670L', () => {
-    let device = new DeviceModel({use_tr069: true, model: 'F670L'});
-    expect(DeviceVersion.checkFeature(device.model, 'wps')).toBe(false);
+  // Flashbox tests
+
+  test('findByVersion on 0.30.0', () => {
+    let permissions = DeviceVersion.findByVersion('0.30.0', true, 'ARCHERC5V4');
+
+    expect(permissions.grantViewLogs).toStrictEqual(true);
+    expect(permissions.grantResetDevices).toStrictEqual(true);
+    expect(permissions.grantPortForward).toStrictEqual(true);
+    expect(permissions.grantPortForwardAsym).toStrictEqual(true);
+    expect(permissions.grantPortOpenIpv6).toStrictEqual(true);
+    expect(permissions.grantWifi5ghz).toStrictEqual(true);
+    expect(permissions.grantWifiBand).toStrictEqual(true);
+    expect(permissions.grantWifiBandAuto).toStrictEqual(true);
+    expect(permissions.grantWifiState).toStrictEqual(true);
+    expect(permissions.grantWifiPowerHiddenIpv6Box).toStrictEqual(true);
+    expect(permissions.grantWifiExtendedChannels).toStrictEqual(false);
+    expect(permissions.grantPingTest).toStrictEqual(true);
+    expect(permissions.grantLanEdit).toStrictEqual(true);
+    expect(permissions.grantLanGwEdit).toStrictEqual(true);
+    expect(permissions.grantLanDevices).toStrictEqual(true);
+    expect(permissions.grantSiteSurvey).toStrictEqual(true);
+    expect(permissions.grantUpnp).toStrictEqual(true);
+    expect(permissions.grantSpeedTest).toStrictEqual(true);
+    expect(permissions.grantSpeedTestLimit).toStrictEqual(300);
+    expect(permissions.grantBlockDevices).toStrictEqual(true);
+    expect(permissions.grantOpmode).toStrictEqual(true);
+    expect(permissions.grantVlanSupport).toStrictEqual(false);
+    expect(permissions.grantWanBytesSupport).toStrictEqual(true);
+    expect(permissions.grantPonSignalSupport).toStrictEqual(false);
+    expect(permissions.grantMeshMode).toStrictEqual(true);
+    expect(permissions.grantUpdateAck).toStrictEqual(true);
+    expect(permissions.grantWpsFunction).toStrictEqual(true);
+    expect(permissions.grantPortForwardOpts).toStrictEqual(undefined);
   });
 
-  test('check feature upnp F670L', () => {
-    let device = new DeviceModel({use_tr069: true, model: 'F670L'});
-    expect(DeviceVersion.checkFeature(device.model, 'upnp')).toBe(false);
-  });
-
-  test('check feature speedTest F670L', () => {
-    let device = new DeviceModel({use_tr069: true, model: 'F670L'});
-    expect(DeviceVersion.checkFeature(device.model, 'speedTest')).toBe(false);
-  });
-
-  test('check feature speedTestLimit F670L', () => {
-    let device = new DeviceModel({use_tr069: true, model: 'F670L'});
-    expect(DeviceVersion.checkFeature(device.model, 'speedTestLimit')).toBe(
-      false,
-    );
-  });
-
-  test('check feature blockDevices F670L', () => {
-    let device = new DeviceModel({use_tr069: true, model: 'F670L'});
-    expect(DeviceVersion.checkFeature(device.model, 'blockDevices')).toBe(
-      false,
-    );
-  });
-
-  test('check feature wps other model', () => {
-    let device = new DeviceModel({use_tr069: true, model: 'GONUAC001'});
-    expect(DeviceVersion.checkFeature(device.model, 'wps')).toBe(false);
-  });
-
-  test('check feature upnp other model', () => {
-    let device = new DeviceModel({use_tr069: true, model: 'GONUAC001'});
-    expect(DeviceVersion.checkFeature(device.model, 'upnp')).toBe(false);
-  });
-
-  test('check feature speedTest other model', () => {
-    let device = new DeviceModel({use_tr069: true, model: 'GONUAC001'});
-    expect(DeviceVersion.checkFeature(device.model, 'speedTest')).toBe(false);
-  });
-
-  test('check feature speedTestLimit other model', () => {
-    let device = new DeviceModel({use_tr069: true, model: 'GONUAC001'});
-    expect(DeviceVersion.checkFeature(device.model, 'speedTestLimit')).toBe(
-      false,
-    );
-  });
-
-  test('check feature blockDevices other model', () => {
-    let device = new DeviceModel({use_tr069: true, model: 'GONUAC001'});
-    expect(DeviceVersion.checkFeature(device.model, 'blockDevices')).toBe(
-      false,
-    );
-  });
-
-  test('without model', () => {
-    expect(DeviceVersion.checkFeature('', 'wps')).toBe(true);
-  });
-
-  test('empty feature variable', () => {
-    let device = new DeviceModel();
-    expect(DeviceVersion.checkFeature(device.model, '')).toBe(true);
-  });
+  // TR-069 tests
 
   test('findByVersion on ZTE F670L', () => {
-    let device = new DeviceModel(zteF670LData);
-    let expectedResult = {
-      grantViewLogs: true,
-      grantResetDevices: true,
-      grantPortForward: false,
-      grantPortForwardAsym: true,
-      grantPortOpenIpv6: true,
-      grantWifi5ghz: true,
-      grantWifiBand: true,
-      grantWifiBandAuto: true,
-      grantWifiState: true,
-      grantWifiPowerHiddenIpv6Box: true,
-      grantPingTest: true,
-      grantLanEdit: true,
-      grantLanGwEdit: true,
-      grantLanDevices: true,
-      grantSiteSurvey: true,
-      grantUpnp: false,
-      grantSpeedTest: false,
-      grantSpeedTestLimit: 0,
-      grantBlockDevices: false,
-      grantOpmode: true,
-      grantVlanSupport: false,
-      grantWanBytesSupport: true,
-      grantPonSignalSupport: true,
-      grantMeshMode: true,
-      grantUpdateAck: false,
-      grantWpsFunction: false,
-    };
-
-    let permissionTest = DeviceVersion.findByVersion(
-      device.version,
-      device.wifi_is_5ghz_capable,
-      device.model,
+    let permissionsP1T4 = DeviceVersion.findByVersion(
+      'V1.1.20P1T4', true, 'F670L',
     );
-    expect(permissionTest).toStrictEqual(expectedResult);
+    let permissionsP1T18 = DeviceVersion.findByVersion(
+      'V1.1.20P1T18', true, 'F670L',
+    );
+    let permissionsP3N3 = DeviceVersion.findByVersion(
+      'V1.1.20P3N3', true, 'F670L',
+    );
+
+    [permissionsP1T4, permissionsP1T18, permissionsP3N3].forEach((permission)=>{
+      expect(permission.grantPortForward).toStrictEqual(true);
+      expect(permission.grantUpnp).toStrictEqual(false);
+      expect(permission.grantWpsFunction).toStrictEqual(false);
+      expect(permission.grantSpeedTest).toStrictEqual(false);
+      expect(permission.grantSpeedTestLimit).toStrictEqual(0);
+      expect(permission.grantBlockDevices).toStrictEqual(false);
+      expect(permission.grantPonSignalSupport).toStrictEqual(true);
+      expect(permission.grantPortForwardOpts).toStrictEqual(
+        noRangePortForwawrdOpts,
+      );
+    });
+  });
+
+  test('findByVersion on ZTE H198A', () => {
+    let permissionsC5 = DeviceVersion.findByVersion(
+      'V3.0.0C5_MUL', true, 'ZXHN H198A V3.0',
+    );
+    let permissionsC6 = DeviceVersion.findByVersion(
+      'V3.0.0C6_MUL', true, 'ZXHN H198A V3.0',
+    );
+
+    [permissionsC5, permissionsC6].forEach((permission)=>{
+      expect(permission.grantPortForward).toStrictEqual(true);
+      expect(permission.grantUpnp).toStrictEqual(false);
+      expect(permission.grantWpsFunction).toStrictEqual(false);
+      expect(permission.grantSpeedTest).toStrictEqual(false);
+      expect(permission.grantSpeedTestLimit).toStrictEqual(0);
+      expect(permission.grantBlockDevices).toStrictEqual(false);
+      expect(permission.grantPonSignalSupport).toStrictEqual(false);
+      expect(permission.grantPortForwardOpts).toStrictEqual(
+        noAsymRangePortForwawrdOpts,
+      );
+    });
+  });
+
+  test('findByVersion on GONUAC001', () => {
+    let permissions123 = DeviceVersion.findByVersion(
+      'V1.2.3', true, 'GONUAC001',
+    );
+
+    [permissions123].forEach((permission)=>{
+      expect(permission.grantPortForward).toStrictEqual(false);
+      expect(permission.grantUpnp).toStrictEqual(false);
+      expect(permission.grantWpsFunction).toStrictEqual(false);
+      expect(permission.grantSpeedTest).toStrictEqual(false);
+      expect(permission.grantSpeedTestLimit).toStrictEqual(0);
+      expect(permission.grantBlockDevices).toStrictEqual(false);
+      expect(permission.grantPonSignalSupport).toStrictEqual(true);
+      expect(permission.grantPortForwardOpts).toStrictEqual(undefined);
+    });
+  });
+
+  test('findByVersion on G-140W-C', () => {
+    let permissionsA89 = DeviceVersion.findByVersion(
+      '3FE46343AFIA89', true, 'G-140W-C',
+    );
+
+    [permissionsA89].forEach((permission)=>{
+      expect(permission.grantPortForward).toStrictEqual(false);
+      expect(permission.grantUpnp).toStrictEqual(false);
+      expect(permission.grantWpsFunction).toStrictEqual(false);
+      expect(permission.grantSpeedTest).toStrictEqual(false);
+      expect(permission.grantSpeedTestLimit).toStrictEqual(0);
+      expect(permission.grantBlockDevices).toStrictEqual(false);
+      expect(permission.grantPonSignalSupport).toStrictEqual(true);
+      expect(permission.grantPortForwardOpts).toStrictEqual(undefined);
+    });
+  });
+
+  test('findByVersion on HG8245Q2', () => {
+    let permissionsV3 = DeviceVersion.findByVersion(
+      'V3R017C10S100', true, 'HG8245Q2',
+    );
+
+    [permissionsV3].forEach((permission)=>{
+      expect(permission.grantPortForward).toStrictEqual(false);
+      expect(permission.grantUpnp).toStrictEqual(false);
+      expect(permission.grantWpsFunction).toStrictEqual(false);
+      expect(permission.grantSpeedTest).toStrictEqual(false);
+      expect(permission.grantSpeedTestLimit).toStrictEqual(0);
+      expect(permission.grantBlockDevices).toStrictEqual(false);
+      expect(permission.grantPonSignalSupport).toStrictEqual(true);
+      expect(permission.grantPortForwardOpts).toStrictEqual(undefined);
+    });
   });
 });
