dcd6f3340bc5c00a7620644e18b60ddc8b58a9b3
W1N3 <gabriel.silva.w1n3@gmail.com>
2021-10-26 17:44:55 -0300

    more 2 test cases for digestXmlConfig

--- test/unit/acs.handlers.test.js
+++ test/unit/acs.handlers.test.js
@@ -125,4 +125,93 @@ describe('TR-069 GenieACS communication methods', () => {
     let finiXml = xmlFailTest(5);
     expect(finiXml).toBe('');
   });
+  // web admin user and password is changed
+  test('digestXmlConfig: web admin user and password is changed', () => {
+    let waUser = 'superuser';
+    let waPassword = 'superpassword';
+    let device = {
+      serial_tr069: '8H3F98AHF9Q38FH',
+      web_admin: {
+        web_login: waUser,
+        web_password: waPassword,
+      },
+    };
+    let initXml = fs.readFileSync('./test/assets/config_file_stavix_1.xml',
+      'utf8');
+    let searchDirNames = new RegExp('Dir Name="[A-Z_]*"', 'g');
+    let initDirNamesList = initXml.match(searchDirNames);
+    let DirNamesMap = new Map();
+    initDirNamesList.forEach((dn) => {
+      DirNamesMap.set(dn);
+    });
+    let finiXml = acsHandlers
+      .digestXmlConfig(device, initXml, ['web-admin']);
+    let searchUser = new RegExp('SUSER_NAME" Value="[0-9A-Za-z_]*');
+    let searchPassword = new RegExp('SUSER_PASSWORD" Value="[0-9A-Za-z_]*');
+    let finiDirNamesList = finiXml.match(searchDirNames);
+    let userList = finiXml.match(searchUser);
+    let passwordList = finiXml.match(searchPassword);
+    let sameDirNamesCheck = finiDirNamesList.every((dn) => {
+      return DirNamesMap.has(dn);
+    });
+    expect(userList[0].includes(waUser)).toBe(true);
+    expect(passwordList[0].includes(waPassword)).toBe(true);
+    expect(sameDirNamesCheck).toBe(true);
+  });
+  // both web-admin and port-forward targets running right
+  test('digestXmlConfig: both web-admin and port-forward'+
+    ' targets running right', () => {
+    let waUser = 'test123';
+    let waPassword = '456test';
+    let device = {
+      serial_tr069: '8H3F98AHF9Q38FH',
+      port_mapping: [{
+        internal_port_start: 2323,
+        internal_port_end: 2323,
+        external_port_start: 2323,
+        external_port_end: 2323,
+        ip: '192.168.1.23',
+      },
+      {
+        internal_port_start: 4540,
+        internal_port_end: 4550,
+        external_port_start: 5440,
+        external_port_end: 5450,
+        ip: '192.168.1.45',
+      }],
+      web_admin: {
+        web_login: waUser,
+        web_password: waPassword,
+      },
+    };
+    let initXml = fs.readFileSync('./test/assets/config_file_stavix_1.xml',
+      'utf8');
+    let searchDirNames = new RegExp('Dir Name="[A-Z_]*"', 'g');
+    let initDirNamesList = initXml.match(searchDirNames);
+    let DirNamesMap = new Map();
+    initDirNamesList.forEach((dn) => {
+      DirNamesMap.set(dn);
+    });
+    let finiXml = acsHandlers
+      .digestXmlConfig(device, initXml, ['port-forward', 'web-admin']);
+    let searchPortFwTbl = new RegExp('Dir Name="PORT_FW_TBL"', 'g');
+    let searchPortFwEnable = new RegExp('PORT_FW_ENABLE" Value="[0-9]');
+    let finiDirNamesList = finiXml.match(searchDirNames);
+    let searchUser = new RegExp('SUSER_NAME" Value="[0-9A-Za-z_]*');
+    let searchPassword = new RegExp('SUSER_PASSWORD" Value="[0-9A-Za-z_]*');
+    let userList = finiXml.match(searchUser);
+    let passwordList = finiXml.match(searchPassword);
+    let pfw = finiXml.match(searchPortFwEnable)[0];
+    let portFwEnableValue = pfw[pfw.length-1];
+    let portFwTblAmount = finiXml.match(searchPortFwTbl).length;
+    let sameDirNamesCheck = finiDirNamesList.every((dn) => {
+      return DirNamesMap.has(dn);
+    });
+
+    expect(sameDirNamesCheck).toBe(true);
+    expect(portFwEnableValue).toBe('1');
+    expect(userList[0].includes(waUser)).toBe(true);
+    expect(passwordList[0].includes(waPassword)).toBe(true);
+    expect(portFwTblAmount).toBe(2);
+  });
 });
