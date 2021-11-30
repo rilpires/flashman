eee5c5c0bf31d37db4343ab39bbeaebfc5918dd1
danielatk <danatkhoo@yahoo.com.br>
2021-11-30 17:15:00 -0300

    increasing sleep between creating mesh VAP objects and fetching mesh bssids

--- controllers/acs_device_info.js
+++ controllers/acs_device_info.js
@@ -1498,7 +1498,7 @@ acsDeviceInfoController.getMeshBSSIDFromGenie = async function(
     getObjTask.parameterNames.push(bssidField5);
   }
   let bssidsStatus;
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise(r => setTimeout(r, 4000));
   try {
     let ret = await TasksAPI.addTask(acsID, getObjTask, true, 10000, []);
     if (!ret || !ret.finished ||
