d62026eb11ffaa07177441dfa708249416e367f6
JotaPe <jotape@protonmail.com>
2022-03-11 21:35:40 -0300

    port forward complete

--- controllers/external-genieacs/devices-api.js
+++ controllers/external-genieacs/devices-api.js
@@ -561,6 +561,11 @@ const getNokiaFields = function(model) {
       fields.devices.host_rssi = 'InternetGatewayDevice.LANDevice.1.WLANConfiguration.*.AssociatedDevice.*.RSSI';
       fields.common.web_admin_username = 'InternetGatewayDevice.X_Authentication.WebAccount.UserName';
       fields.common.web_admin_password = 'InternetGatewayDevice.X_Authentication.WebAccount.Password';
+      fields.port_mapping_values.protocol[1] = 'TCPorUDP';
+      fields.port_mapping_fields.external_port_end = ['ExternalPortEndRange', 'external_port_end', 'xsd:unsignedInt'];
+      fields.port_mapping_values.remote_host =
+         ['RemoteHost', '', 'xsd:string'];
+      fields.port_mapping_fields.internal_port_end = ['X_ASB_COM_InternalPortEnd', 'internal_port_end', 'xsd:unsignedInt'];
       break;
     case 'G-140W-C':
     case 'G%2D140W%2DC':
