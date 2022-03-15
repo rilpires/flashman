f2a7d9fa6593282c5cbb1ea4a1672320ca714376
Thurler <gt@anlix.io>
2022-03-14 21:05:59 -0300

    Fixed bug where 5ghz band configuration was not working for ac modes

--- controllers/acs_device_info.js
+++ controllers/acs_device_info.js
@@ -125,8 +125,8 @@ const convertToDbm = function(model, rxPower) {
   }
 };
 
-const convertWifiBand = function(band, mode) {
-  let isAC = convertWifiMode(mode) === '11ac';
+const convertWifiBand = function(band, mode, is5ghz) {
+  let isAC = convertWifiMode(mode, is5ghz) === '11ac';
   switch (band) {
     case '2':
     case 'auto':
@@ -393,7 +393,7 @@ const createRegistry = async function(req, permissions) {
     wifi_mode: (data.wifi2.mode) ?
       convertWifiMode(data.wifi2.mode.value, false) : undefined,
     wifi_band: (data.wifi2.band) ?
-      convertWifiBand(data.wifi2.band.value, data.wifi2.mode.value) :
+      convertWifiBand(data.wifi2.band.value, data.wifi2.mode.value, false) :
        undefined,
     wifi_state: (data.wifi2.enable.value) ? 1 : 0,
     wifi_is_5ghz_capable: wifi5Capable,
@@ -404,7 +404,7 @@ const createRegistry = async function(req, permissions) {
     wifi_mode_5ghz: (data.wifi5.mode) ?
       convertWifiMode(data.wifi5.mode.value, true) : undefined,
     wifi_band_5ghz: (data.wifi5.band) ?
-      convertWifiBand(data.wifi5.band.value, data.wifi5.mode.value) :
+      convertWifiBand(data.wifi5.band.value, data.wifi5.mode.value, true) :
        undefined,
     wifi_state_5ghz: (wifi5Capable && data.wifi5.enable.value) ? 1 : 0,
     lan_subnet: data.lan.router_ip.value,
@@ -764,7 +764,7 @@ acsDeviceInfoController.syncDevice = async function(req, res) {
   }
   if (data.wifi2.band) {
     let band2 = convertWifiBand(data.wifi2.band.value,
-     data.wifi2.mode.value);
+     data.wifi2.mode.value, false);
     if (data.wifi2.band.value && !device.wifi_band) {
       device.wifi_band = band2;
     } else if (device.wifi_band !== band2) {
@@ -825,7 +825,7 @@ acsDeviceInfoController.syncDevice = async function(req, res) {
   }
   if (data.wifi5.band && data.wifi5.mode) {
     let band5 = convertWifiBand(data.wifi5.band.value,
-     data.wifi5.mode.value);
+     data.wifi5.mode.value, true);
     if (data.wifi5.band.value && !device.wifi_band_5ghz) {
       device.wifi_band_5ghz = band5;
     } else if (device.wifi_band_5ghz !== band5) {
