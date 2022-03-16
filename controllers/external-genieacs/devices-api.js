0dba23ea4e93f09cb2a595e18f516e2f4aabef5f
Julio Paiva <julio.paiva@anlix.io>
2022-03-16 16:56:18 -0300

    Adding Nokia G-2425G-A to flashman

--- controllers/external-genieacs/devices-api.js
+++ controllers/external-genieacs/devices-api.js
@@ -65,24 +65,27 @@ const convertWifiMode = function(mode, oui, model) {
   let ouiModelStr = model;
   switch (mode) {
     case '11g':
-      if (ouiModelStr === 'IGD' || ouiModelStr === 'FW323DAC') return 'b,g';
-      else if (
+      if (
         ouiModelStr === 'G%2D140W%2DC' ||
         ouiModelStr === 'G%2D140W%2DCS' ||
-        ouiModelStr === 'G%2D140W%2DUD'
+        ouiModelStr === 'G%2D140W%2DUD' ||
+        ouiModelStr === 'HG9'
       ) {
         return 'g';
-      } else if (ouiModelStr === 'F670L') return 'b,g';
-      else if (ouiModelStr === 'F680') return 'b,g';
-      else if (ouiModelStr === 'F660') return 'b,g';
-      else if (ouiModelStr === 'HG9') return 'g';
-      else if (ouiModelStr === 'HG8245Q2') return '11bg';
+      }  else if (ouiModelStr === 'HG8245Q2') return '11bg';
       else if (ouiModelStr === 'Huawei') return 'b/g';
       else if (ouiModelStr === 'AC10') return 'bg';
       else if (
+        ouiModelStr === 'IGD' ||
+        ouiModelStr === 'FW323DAC' ||
+        ouiModelStr === 'F670L' ||
+        ouiModelStr === 'F680' ||
+        ouiModelStr === 'F660' ||
         ouiModelStr === 'G-140W-C' ||
         ouiModelStr === 'G-140W-CS' ||
-        ouiModelStr === 'G-140W-UD'
+        ouiModelStr === 'G-140W-UD' ||
+        ouiModelStr == 'G-2425G-A' ||
+        ouiModelStr == 'G%2D2425G%2DA'
       ) {
         return 'b,g';
       } else if (ouiModelStr === 'GONUAC001') return 'bg';
@@ -91,8 +94,7 @@ const convertWifiMode = function(mode, oui, model) {
       }
       else return '11bg';
     case '11n':
-      if (ouiModelStr === 'IGD' || ouiModelStr === 'FW323DAC') return 'b,g,n';
-      else if (
+      if (
         ouiModelStr === 'G%2D140W%2DC' ||
         ouiModelStr === 'G%2D140W%2DCS' ||
         ouiModelStr === 'G%2D140W%2DUD'
@@ -100,21 +102,24 @@ const convertWifiMode = function(mode, oui, model) {
         return 'n';
       } else if (ouiModelStr === 'HG8245Q2') return '11bgn';
       else if (ouiModelStr === 'Huawei') return 'b/g/n';
-      else if (ouiModelStr === 'F670L') return 'b,g,n';
-      else if (ouiModelStr === 'F660') return 'b,g,n';
-      else if (ouiModelStr === 'F680') return 'b,g,n';
       else if (ouiModelStr === 'HG9') return 'gn';
       else if (ouiModelStr === 'AC10') return 'bgn';
       else if (
+        ouiModelStr === 'IGD' ||
+        ouiModelStr === 'FW323DAC' ||
+        ouiModelStr === 'F670L' ||
+        ouiModelStr === 'F660' ||
+        ouiModelStr === 'F680' ||
         ouiModelStr === 'G-140W-C' ||
         ouiModelStr === 'G-140W-CS' ||
-        ouiModelStr === 'G-140W-UD'
+        ouiModelStr === 'G-140W-UD' ||
+        ouiModelStr === 'DIR-842' ||
+        ouiModelStr === 'DIR-841' ||
+        ouiModelStr == 'G-2425G-A' ||
+        ouiModelStr == 'G%2D2425G%2DA'
       ) {
         return 'b,g,n';
       } else if (ouiModelStr === 'GONUAC001') return 'bgn';
-      else if (ouiModelStr === 'DIR-842' || ouiModelStr === 'DIR-841') {
-        return 'b,g,n';
-      }
       else return '11bgn';
     case '11na':
       if (ouiModelStr === 'IGD' || ouiModelStr === 'FW323DAC') return 'a,n';
@@ -140,6 +145,11 @@ const convertWifiMode = function(mode, oui, model) {
       } else if (ouiModelStr === 'GONUAC001') return 'an';
       else if (ouiModelStr === 'DIR-842' || ouiModelStr === 'DIR-841') {
         return 'a,n';
+      } else if (
+        ouiModelStr == 'G-2425G-A' ||
+        ouiModelStr == 'G%2D2425G%2DA'
+      ) {
+        return 'a,n,ac';
       }
       else return '11na';
     case '11ac':
@@ -152,15 +162,17 @@ const convertWifiMode = function(mode, oui, model) {
         return 'ac';
       } else if (ouiModelStr === 'HG8245Q2') return '11ac';
       else if (ouiModelStr === 'Huawei') return 'a/n/ac';
-      else if (ouiModelStr === 'F670L') return 'a,n,ac';
-      else if (ouiModelStr === 'F660') return 'a,n,ac';
-      else if (ouiModelStr === 'F680') return 'a,n,ac';
       else if (ouiModelStr === 'HG9') return 'gn';
       else if (ouiModelStr === 'AC10') return 'an+ac';
       else if (
+        ouiModelStr === 'F670L' ||
+        ouiModelStr === 'F660' ||
+        ouiModelStr === 'F680' ||
         ouiModelStr === 'G-140W-C' ||
         ouiModelStr === 'G-140W-CS' ||
-        ouiModelStr === 'G-140W-UD'
+        ouiModelStr === 'G-140W-UD' ||
+        ouiModelStr == 'G-2425G-A' ||
+        ouiModelStr == 'G%2D2425G%2DA'
       ) {
         return 'a,n,ac';
       } else if (ouiModelStr === 'GONUAC001') return 'anac';
@@ -174,10 +186,14 @@ const convertWifiMode = function(mode, oui, model) {
 };
 
 const convertWifiBand = function(band, model, is5ghz=false) {
+  if ((model == 'G-2425G-A' || model == 'G%2D2425G%2DA') && !is5ghz) {
+    return '20MHz'
+  }
   switch (band) {
     case 'HT20':
     case 'VHT20':
-      return ((model === 'AC10') ? '0' : '20MHz');
+      if (model === 'AC10') return '0';
+      return '20MHz';
     case 'HT40':
     case 'VHT40':
       if (model === 'DIR-842' || model === 'DIR-841') return '20/40MHz';
@@ -190,14 +206,18 @@ const convertWifiBand = function(band, model, is5ghz=false) {
     case 'auto':
       if (model === 'DIR-842' || model === 'DIR-841') {
         return (is5ghz) ? '20/40/80MHz' : '20/40MHz Coexistence';
-      }
-      if (
+      } else if (
         model === 'BEACON 1 HA-020W-B' ||
         model === 'BEACON%20HA%2D020W%2DB'
       ) {
         return 'Auto'
+      } else if (model === 'AC10') {
+        return '2';
+      } else if (
+        model == 'G-2425G-A' || model == 'G%2D2425G%2DA'
+      ) {
+        return '80MHz'
       }
-      else if (model === 'AC10') return '2';
       return 'auto';
     default:
       return '';
@@ -556,12 +576,21 @@ const getZTEFields = function(model) {
   return fields;
 };
 
+const getNokiaG2425Fields = function(model) {
+  let fields = getNokiaFields(model);
+  fields.wan.recv_bytes = 'InternetGatewayDevice.WANDevice.1.WANCommonInterfaceConfig.TotalBytesReceived';
+  fields.wan.sent_bytes = 'InternetGatewayDevice.WANDevice.1.WANCommonInterfaceConfig.TotalBytesSent';
+  return fields;
+}
+
 const getNokiaFields = function(model) {
   let fields = getDefaultFields();
   switch (model) {
     case 'BEACON HA-020W-B':
     case 'BEACON 1 HA-020W-B':
     case 'BEACON%20HA%2D020W%2DB':
+    case 'G-2425G-A':
+    case 'G%2D2425G%2DA': // URI encoded
       fields.wifi2.band = 'InternetGatewayDevice.LANDevice.1.WLANConfiguration.1.X_ALU_COM_ChannelBandWidthExtend';
       fields.wifi2.password = fields.wifi2.password.replace(/KeyPassphrase/g, 'PreSharedKey.1.KeyPassphrase');
       fields.wifi5.band = 'InternetGatewayDevice.LANDevice.1.WLANConfiguration.5.X_ALU_COM_ChannelBandWidthExtend';
@@ -873,6 +902,11 @@ const getModelFields = function(oui, model, modelName, firmwareVersion) {
     case 'BEACON%20HA%2D020W%2DB': // URI encoded
       message = '';
       fields = getNokiaFields(model);
+      break;
+    case 'G-2425G-A':
+    case 'G%2D2425G%2DA': // URI encoded
+      message = '';
+      fields = getNokiaG2425Fields(model);
       break;
     case 'MP_G421R': // Unee Stavix G412R
     case 'xPON': // Intelbras WiFiber (is a Stavix clone)
--- models/device_version.js
+++ models/device_version.js
@@ -386,6 +386,35 @@ const tr069Devices = {
     mesh5_bssid_offset: ['0x0', '0x0', '0x0', '0x0', '0x0', '0x1'],
     mesh_ssid_object_exists: true,
   },
+  'G-2425G-A': {
+    vendor: 'Nokia',
+    versions_upgrade: {
+      '3FE49025IJHK03': [],
+    },
+    port_forward_opts: {
+      '3FE49025IJHK03': portForwardFullSupport,
+    },
+    feature_support: {
+      port_forward: true,
+      pon_signal: true, // no fiber
+      upnp: false,
+      speed_test: true,
+      speed_test_limit: 10000,
+      ping_test: true,
+      block_devices: false,
+      firmware_upgrade: false,
+      mesh_v2_primary_support: false,
+      mesh_v2_secondary_support: false,
+    },
+    wifi2_extended_channels_support: false,
+    mesh_bssid_offset_hardcoded: false,
+    // offset of each BSSID octet in relation
+    // to the MAC address (first element corresponds to
+    // offset of the leftmost octet, and so forth)
+    // mesh2_bssid_offset: ['0x0', '0x0', '0x0', '0x0', '0x0', '0x6'],
+    // mesh5_bssid_offset: ['0x0', '0x0', '0x0', '0x0', '0x0', '0x1'],
+    // mesh_ssid_object_exists: false,
+  },
   '121AC': {
     vendor: 'Intelbras',
     versions_upgrade: {
