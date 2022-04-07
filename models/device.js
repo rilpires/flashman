796bf0da945bad4ffeaa29e545f5d217839481a4
cassiohg <cassiohg@gmail.com>
2022-04-07 19:24:24 -0300

    Added comment to model/device concerning localization.

--- models/device.js
+++ models/device.js
@@ -21,6 +21,7 @@ let deviceSchema = new Schema({
   recovering_tr069_reset: {type: Boolean, default: false},
   created_at: {type: Date},
   external_reference: {
+    // 'kind' will have to be translated according to region.
     kind: {type: String, enum: ['CPF', 'CNPJ', 'Outro']},
     data: String,
   },
@@ -30,9 +31,9 @@ let deviceSchema = new Schema({
   release: String,
   is_license_active: Boolean,
   data_collecting: {
-    is_active: Boolean, // logical AND with config.js value
-    has_latency: Boolean, // logical AND with config.js value
-    // should use config.js value if this value is falsifiable
+    is_active: Boolean, // logical AND with config.js value.
+    has_latency: Boolean, // logical AND with config.js value.
+    // should use config.js value if this value is falsifiable.
     ping_fqdn: String,
   },
   connection_type: {type: String, enum: ['pppoe', 'dhcp']},
