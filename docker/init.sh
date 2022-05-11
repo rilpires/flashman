01b941eb9c9cb2bd80f5187591649ba25285cdcc
W1N3 <gabriel.silva.w1n3@gmail.com>
2022-05-11 16:03:51 -0300

    Imbue wait for it inside initialization because wait-for-it script does not work in ash context + init cron to refresh certs

--- docker/init.sh
+++ docker/init.sh
@@ -1,17 +1,19 @@
-#!/usr/bin/env bash
-./wait-for-it.sh courier:2332 -t 0 -- echo 'Courier is ready...'
-echo 'Retrieving certs...'
-rm -rf /app/certs
-cd /app
+#!/bin/sh
+./wait-for-courier.sh
+
+# wait for mongo
+start_ts=$(date +%s)
 while :
 do
-    sleep 15
-    wget -r --no-parent --reject "index.html*" -nd -P certs 'http://courier:2332/files/certs' >/dev/null 2>/dev/null
-    ret=$?
-    echo $ret
-    [[ ! -d '/app/certs' ]] || break
+    nc -z ${FLM_MONGODB_HOST} ${FLM_MONGODB_PORT}
+    result=$?
+    if [[ $result -eq 0 ]]; then
+        end_ts=$(date +%s)
+        echo "$FLM_MONGODB_HOST:$FLM_MONGODB_PORT is available after $((end_ts - start_ts)) seconds"
+        break
+    fi
+    sleep 1
 done
-find /app/certs -type d -exec chmod 755 {} \;
-find /app/certs -type f -exec chmod 400 {} \;
-echo 'Certs succesfully downloaded...'
-./wait-for-it.sh ${FLM_MONGODB_HOST}:${FLM_MONGODB_PORT} -t 0 -- node bin/www
+# init cron for daily update certs
+crond -l 8
+node bin/www
