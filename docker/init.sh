88ab97bfc13c93b29a3931f45647edc1dd807f3a
W1N3 <gabriel.silva.w1n3@gmail.com>
2022-06-03 14:28:18 -0300

    Wait for NBI before initialize flashman to have warranty of avaiability of response to update routines associated with provision and presets files

--- docker/init.sh
+++ docker/init.sh
@@ -14,6 +14,21 @@ do
     fi
     sleep 1
 done
+
+# wait for nbi
+start_ts=$(date +%s)
+while :
+do
+    nc -z ${FLM_NBI_ADDR} ${FLM_NBI_PORT}
+    result=$?
+    if [[ $result -eq 0 ]]; then
+        end_ts=$(date +%s)
+        echo "$FLM_NBI_ADDR:$FLM_NBI_PORT is available after $((end_ts - start_ts)) seconds"
+        break
+    fi
+    sleep 1
+done
+
 # init cron for daily update certs
 crond -l 8 && echo 'cron inited...'
 node bin/www
