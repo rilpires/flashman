a39562ab9350bfa9b0e5dd9d3a4f5d436097e938
Romeu Pires <rilpires@gmail.com>
2022-06-17 10:28:25 -0300

    Removing unnecessary routes for device_list

--- routes/device_list.js
+++ routes/device_list.js
@@ -93,16 +93,6 @@ router.route('/command/:id/:msg').post(
   authController.ensurePermission('grantDeviceActions'),
   deviceListController.sendMqttMsg);
 
-// Send a customized ping command
-router.route('/pingdiagnostic/:id').post(
-  authController.ensurePermission('grantDeviceActions'),
-  deviceListController.sendCustomPing);
-
-// Send a customized speedtest command
-router.route('/device/speeddiagnostic/:id').post(
-  authController.ensurePermission('grantDeviceActions'),
-  deviceListController.sendCustomSpeedTest);
-
 // For user Interface - Set/Get Port forward
 router.route('/uiportforward/:id').get(
   deviceListController.getPortForward)
