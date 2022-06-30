f38557bec0468401b2ecaeaff2fb979628005d2b
Julio Paiva <julio.paiva@anlix.io>
2022-06-30 16:02:30 -0300

    Mocking devices credentials info

--- controllers/app_diagnostic_api.js
+++ controllers/app_diagnostic_api.js
@@ -20,6 +20,57 @@ const t = require('./language').i18next.t;
 
 let diagAppAPIController = {};
 
+// Add onu custom credentials, if present
+let onuFactoryCredentials = {
+  timestamp: new Date(),
+  credentials: [
+    {vendor: 'D-Link', model: 'DIR-841', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'D-Link', model: 'DIR-842', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'FastWireless', model: 'FW323DAC', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Greatek', model: 'Stavix G421RQ', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Huawei', model: 'EG8145V5', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Huawei', model: 'EG8145X6', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Huawei', model: 'HG8245Q2', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Huawei', model: 'WS5200', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Huawei', model: 'WS7001 / AX2', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Huawei', model: 'WS7100 / AX3', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Hurakall', model: 'ST-1001-FL', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Intelbras', model: 'WiFiber 121 AC', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Multilaser / ZTE', model: 'F660', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Multilaser / ZTE', model: 'F670L', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Multilaser / ZTE', model: 'F680', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Multilaser / ZTE', model: 'H198A', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Multilaser / ZTE', model: 'H199A', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Nokia', model: 'BEACON HA-020W-B', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Nokia', model: 'G-140W-C', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Nokia', model: 'G-2425G-A', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Tenda', model: 'AC10', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'Tenda', model: 'HG9', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'TP-Link', model: 'Archer C6 v3.2', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'TP-Link', model: 'EC220-G5 v2', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'UNEE', model: 'Stavix MPG421R', username: 'admin', password: 'A@Nlix123'},
+    {vendor: 'ZTE', model: 'F670L', username: 'admin', password: 'A@Nlix123'},
+  ],
+};
+
+// TODO: Usar esse objeto para compor os dropdowns na tela de adição de preset
+// let credentialsDropdownObj = {
+//   'D-Link': ['DIR-841', 'DIR-842'],
+//   'FastWireless': ['FW323DAC'],
+//   'Greatek': ['Stavix G421RQ'],
+//   'Huawei': [
+//     'EG8145V5', 'EG8145X6', 'HG8245Q2', 'WS5200', 'WS7001 / AX2', 'WS7100 / AX3'
+//   ],
+//   'Hurakall': ['ST-1001-FL'],
+//   'Intelbras': ['WiFiber 121 AC'],
+//   'Multilaser / ZTE': ['F660', 'F670L', 'F680', 'H198A', 'H199A'],
+//   'Nokia': ['BEACON HA-020W-B', 'G-140W-C', 'G-2425G-A'],
+//   'Tenda': ['AC10', 'HG9'],
+//   'TP-Link': ['Archer C6 v3.2', 'EC220-G5 v2'],
+//   'UNEE': ['Stavix MPG421R'],
+//   'ZTE': ['F670L'],
+// };
+
 const convertDiagnostic = function(diagnostic) {
   return {
     wan: (diagnostic && diagnostic.wan === 0),
@@ -142,15 +193,7 @@ const generateSessionCredential = async (user) => {
     session.onuUserPassword = trConf.web_password_user || '';
     session.onuRemote = trConf.remote_access;
   }
-  // Add onu custom credentials, if present
-  onu_factory_credentials = [
-    {model: 'G-2425G-A', username: 'TESTE1_Username', password: 'TESTE1_Password'},
-    {model: 'TESTE2_Model', username: 'TESTE2_Username', password: 'TESTE2_Password'},
-    {model: 'TESTE3_Model', username: 'TESTE3_Username', password: 'TESTE3_Password'},
-    {model: 'TESTE4_Model', username: 'TESTE4_Username', password: 'TESTE4_Password'},
-    {model: 'TESTE5_Model', username: 'TESTE5_Username', password: 'TESTE5_Password'},
-  ];
-  session.onuFactoryCredentials = onu_factory_credentials;
+  session.onuFactoryCredentials = onuFactoryCredentials;
   return session;
 };
 
@@ -592,15 +635,8 @@ diagAppAPIController.verifyFlashman = async (req, res) => {
         onuConfig.onuPonThresholdCriticalHigh =
           config.tr069.pon_signal_threshold_critical_high;
       }
-      config.onu_factory_credentials = [
-        {model: 'G-2425G-A', username: 'TESTE1_Username', password: 'TESTE1_Password'},
-        {model: 'TESTE2_Model', username: 'TESTE2_Username', password: 'TESTE2_Password'},
-        {model: 'TESTE3_Model', username: 'TESTE3_Username', password: 'TESTE3_Password'},
-        {model: 'TESTE4_Model', username: 'TESTE4_Username', password: 'TESTE4_Password'},
-        {model: 'TESTE5_Model', username: 'TESTE5_Username', password: 'TESTE5_Password'},
-      ];
-      if (config.onu_factory_credentials) {
-        onuConfig.onuFactoryCredentials = config.onu_factory_credentials;
+      if (onuFactoryCredentials) {
+        onuConfig.onuFactoryCredentials = onuFactoryCredentials;
       }
       if (!device) {
         return res.status(200).json({
