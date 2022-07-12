388e38c5dcd9052a2dc07f69ad9168afdadafe43
W1N3 <gabriel.silva.w1n3@gmail.com>
2022-07-12 17:19:01 -0300

    Major fields synced, lack port foward and ping/speed test

--- controllers/external-genieacs/cpe-models/tk-onu-ac-d.js
+++ controllers/external-genieacs/cpe-models/tk-onu-ac-d.js
@@ -2,28 +2,30 @@ const basicCPEModel = require('./base-model');
 
 let tkOnuAcDModel = Object.assign({}, basicCPEModel);
 
-tkOnuAcDModel.identifier = 'Think/TK-ONU-AC-D';
+tkOnuAcDModel.identifier = 'Think TK-ONU-AC-D';
 
 tkOnuAcDModel.modelPermissions = function() {
   let permissions = basicCPEModel.modelPermissions();
-  permissions.features.firmwareUpgrade = true;
+  permissions.features.firmwareUpgrade = false;
   permissions.features.pingTest = true;
+  permissions.features.speedTest = true;
   permissions.features.ponSignal = true;
   permissions.features.portForward = true;
-  permissions.lan.blockLANDevices = true;
   permissions.wan.portForwardPermissions =
     basicCPEModel.portForwardPermissions.noAsymRanges;
-  permissions.wifi.dualBand = false;
+  permissions.wan.speedTestLimit = 2000;
+  permissions.wan.pingTestSingleAttempt = true;
   permissions.firmwareUpgrades = {
     'V1.0.9': [],
   };
+  permissions.wifi.extended2GhzChannels = false;
   return permissions;
 };
 
 tkOnuAcDModel.convertWifiMode = function(mode) {
   switch (mode) {
     case '11g':
-      return 'b,g';
+      return 'g';
     case '11n':
       return 'b,g,n';
     case '11na':
@@ -36,9 +38,37 @@ tkOnuAcDModel.convertWifiMode = function(mode) {
   }
 };
 
-tkOnuAcDModel.getBeaconType = function() {
-  return 'WPAand11i';
+// Conversion from Flashman format to CPE format
+basicCPEModel.convertWifiBand = function(band, is5ghz=false) {
+  switch (band) {
+    case 'HT20':
+    case 'VHT20':
+      return '20Mhz';
+    case 'HT40':
+    case 'VHT40':
+      return '40Mhz';
+    case 'VHT80':
+      return '80Mhz';
+    case 'auto':
+      return 'Auto';
+    default:
+      return '';
+  }
 };
+
+tkOnuAcDModel.getBeaconType = function() {
+  return 'WPA/WPA2';
+};
+
+tkOnuAcDModel.convertChannelToTask = function(channel, fields, masterKey) {
+  let auto = (channel === 'auto');
+  let values = [];
+  values.push([
+    fields[masterKey]['channel'], (auto) ? '0' : channel, 'xsd:unsignedInt',
+  ]);
+  return values;
+};
+
 
 tkOnuAcDModel.convertToDbm = function(power) {
   return parseFloat((10 * Math.log10(power * 0.0001)).toFixed(3));
@@ -50,6 +80,20 @@ tkOnuAcDModel.convertWifiRate = function(rate) {
 
 tkOnuAcDModel.getModelFields = function() {
   let fields = basicCPEModel.getModelFields();
+  fields.wan.vlan = 'InternetGatewayDevice.WANDevice.1.WANConnectionDevice'+
+    '.1.X_CT-COM_WANGponLinkConfig.VLANIDMark';
+  fields.wan.pon_rxpower = 'InternetGatewayDevice.WANDevice.1.'+
+    'X_CT-COM_GponInterfaceConfig.RXPower';
+  fields.wan.pon_txpower = 'InternetGatewayDevice.WANDevice.1.'+
+    'X_CT-COM_GponInterfaceConfig.TXPower';
+  fields.wan.rate = 'InternetGatewayDevice.WANDevice.1.'+
+    'WANCommonInterfaceConfig.Layer1DownstreamMaxBitRate';
+  fields.wan.recv_bytes = 'InternetGatewayDevice.WANDevice.1.'+
+    'WANCommonInterfaceConfig.TotalBytesReceived';
+  fields.wan.sent_bytes = 'InternetGatewayDevice.WANDevice.1.'+
+    'WANCommonInterfaceConfig.TotalBytesSent';
+  fields.common.alt_uid = 'InternetGatewayDevice.LANDevice.1.' +
+    'LANEthernetInterfaceConfig.1.MACAddress';
   return fields;
 };
 
