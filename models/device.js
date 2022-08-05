6e865ed7453f22240b9eb3e83d636ad54a694dfc
Julio Paiva <julio.paiva@anlix.io>
2022-08-05 18:57:43 -0300

    Adding projection to findByMacOrSerial

--- models/device.js
+++ models/device.js
@@ -329,7 +329,9 @@ deviceSchema.methods.getAPSurveyDevice = function(mac) {
   });
 };
 
-deviceSchema.statics.findByMacOrSerial = function(id, useLean=false) {
+deviceSchema.statics.findByMacOrSerial = function(
+  id, useLean=false, projection=null,
+) {
   let query;
   if (Array.isArray(id)) {
     let regexList = [];
@@ -348,12 +350,14 @@ deviceSchema.statics.findByMacOrSerial = function(id, useLean=false) {
     return this.find({$or: [
       {'_id': query}, // mac address
       {'serial_tr069': query}, // serial
-      {'alt_uid_tr069': query}]}).lean(); // mac address
+      {'alt_uid_tr069': query}]}, // mac address
+      projection).lean();
   } else {
     return this.find({$or: [
       {'_id': query}, // mac address
       {'serial_tr069': query}, // serial
-      {'alt_uid_tr069': query}]}); // mac address
+      {'alt_uid_tr069': query}]}, // mac address
+      projection);
   }
 };
 
