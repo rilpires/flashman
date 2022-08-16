4fc51838c3f610911f84cb0e98037339ef7be30e
Thurler <gt@anlix.io>
2022-08-16 11:49:39 -0300

    Removed channel 165 from ZT199 model

--- controllers/external-genieacs/cpe-models/zte-zt199.js
+++ controllers/external-genieacs/cpe-models/zte-zt199.js
@@ -20,7 +20,7 @@ zteModel.modelPermissions = function() {
   permissions.wifi.list5ghzChannels = [
     36, 40, 44, 48, 52, 56, 60, 64,
     100, 104, 108, 112, 116, 120, 124, 128,
-    149, 153, 157, 161, 165,
+    149, 153, 157, 161,
   ];
   permissions.wifi.bandAuto5 = false;
   permissions.mesh.objectExists = true;
