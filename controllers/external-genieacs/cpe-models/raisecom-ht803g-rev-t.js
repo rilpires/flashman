44844031d97673b7f411748cb4d27d9cc398a7a5
Julio Paiva <julio.paiva@anlix.io>
2022-08-17 17:20:29 -0300

    Removing duplicated declaration

--- controllers/external-genieacs/cpe-models/raisecom-ht803g-rev-t.js
+++ controllers/external-genieacs/cpe-models/raisecom-ht803g-rev-t.js
@@ -102,25 +102,6 @@ raisecomModel.convertWifiBandToFlashman = function(band, isAC) {
   }
 };
 
-raisecomModel.getModelFields = function() {
-  let fields = basicCPEModel.getModelFields();
-  fields.wan.recv_bytes = 'InternetGatewayDevice.WANDevice.1.'+
-    'WANCommonInterfaceConfig.TotalBytesReceived';
-  fields.wan.sent_bytes = 'InternetGatewayDevice.WANDevice.1.'+
-    'WANCommonInterfaceConfig.TotalBytesSent';
-  fields.wan.pon_rxpower = 'InternetGatewayDevice.WANDevice.1.'+
-    'X_CT-COM_GponInterfaceConfig.RXPower';
-  fields.wan.pon_txpower = 'InternetGatewayDevice.WANDevice.1.'+
-    'X_CT-COM_GponInterfaceConfig.TXPower';
-  fields.devices.host_layer2 = 'InternetGatewayDevice.LANDevice.1.Hosts.Host.'+
-    '*.InterfaceType';
-  fields.common.web_admin_password = 'InternetGatewayDevice.DeviceInfo.'+
-    'X_CT-COM_TeleComAccount.Password';
-  fields.port_mapping_values.protocol[1] = 'BOTH';
-  delete fields.port_mapping_fields.external_port_end;
-  return fields;
-};
-
 raisecomModel.convertChannelToTask = function(channel, fields, masterKey) {
   if (channel === 'auto') {
     channel = '0';
