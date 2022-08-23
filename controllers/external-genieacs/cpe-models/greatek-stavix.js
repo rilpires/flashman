8543c10b0d300126b472789eb9af3781584cdd55
naccaratocarolina] <naccaratocarolina@gmail.com>
2022-08-23 14:03:44 -0300

    Changes to Greatek Stavix

--- controllers/external-genieacs/cpe-models/greatek-stavix.js
+++ controllers/external-genieacs/cpe-models/greatek-stavix.js
@@ -22,6 +22,7 @@ greatekModel.modelPermissions = function() {
   permissions.wifi.bandAuto2 = false;
   permissions.wifi.bandAuto5 = false;
   permissions.usesStavixXMLConfig = true;
+  permissions.lan.canTrustActive = false;
   permissions.firmwareUpgrades = {
     'V1.2.3': [],
     'V2.2.0': [],
