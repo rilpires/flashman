0a0b463c8539c5d25ba216a40a3cc3fb91e4c216
Thurler <gt@anlix.io>
2022-08-25 16:16:42 -0300

    Adjusted speedtest limit

--- controllers/external-genieacs/cpe-models/fiberhome-hg6143d.js
+++ controllers/external-genieacs/cpe-models/fiberhome-hg6143d.js
@@ -10,7 +10,7 @@ fiberhomeModel.modelPermissions = function() {
   permissions.features.pingTest = true;
   permissions.features.speedTest = true;
   permissions.lan.canTrustActive = true;
-  permissions.wan.speedTestLimit = 730;
+  permissions.wan.speedTestLimit = 700;
   permissions.features.portForward = true;
   permissions.wan.portForwardPermissions =
     basicCPEModel.portForwardPermissions.noAsymRanges;
