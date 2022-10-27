e2cdb123128dc96566ce29754cdeac2e356482a0
Thurler <gt@anlix.io>
2022-10-27 15:03:35 -0300

    Removed unused code that should have been removed long ago

--- bin/globals.js
+++ bin/globals.js
@@ -31,14 +31,3 @@ get: function() {
     return [__stack[1].getFunctionName(), __stack[1].getLineNumber()];
   },
 });
-
-// Returns the resolved value and the error value at the same time.
-// Assign them like this variables in the current scope:
-// let [value, error] = await go(PROMISE);
-global.go = function(p) {
-  // if p is not a Promise, we make it a promise that resolves to itself.
-  if (!(p instanceof Promise)) p = Promise.resolve(p);
-  // if there's no error, error will be undefined.
-  // if there's any error, value will be undefined.
-  return p.then((x) => [x, undefined], (e) => [undefined, e]);
-};
