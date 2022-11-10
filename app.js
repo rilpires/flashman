b6624de64bf74ce64720d9d3a1fa8768c22c1648
Thurler <gt@anlix.io>
2022-11-10 18:58:31 -0300

    Docker compose starts counting instances from 1, subtract to fix 0 comparisons

--- app.js
+++ app.js
@@ -35,8 +35,11 @@ app.locals.appVersion = packageJson.version;
 const MONGOHOST = (process.env.FLM_MONGODB_HOST || 'localhost');
 const MONGOPORT = (process.env.FLM_MONGODB_PORT || 27017);
 
-const instanceNumber = parseInt(process.env.NODE_APP_INSTANCE ||
+let instanceNumber = parseInt(process.env.NODE_APP_INSTANCE ||
                               process.env.FLM_DOCKER_INSTANCE || 0);
+if (process.env.FLM_DOCKER_INSTANCE && instanceNumber > 0) {
+  instanceNumber = instanceNumber - 1; // Docker swarm starts counting at 1
+}
 
 const databaseName = process.env.FLM_DATABASE_NAME === undefined ?
   'flashman' :
