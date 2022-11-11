9ff703b7c7dc61b68530e7001f4fd2140d062553
W1N3 <gabriel.silva.w1n3@gmail.com>
2022-11-11 15:58:08 -0300

    Tests for validateMtu

--- test/unit/device_validator.test.js
+++ test/unit/device_validator.test.js
@@ -2,11 +2,10 @@ require('../../bin/globals.js');
 const Validator = require('../../public/javascripts/device_validator');
 
 describe('Validate functions used both in front and back end)', () => {
-  /*  mtuField(7) - int valid, int invalid 1,
-      int invalid 2, float, string, undefined, null
-      isPPPoE(7) - true, false, 0, 1, '0', '1', 'true',
-      'false', string, undefined, null
-      total tests = 14 */
+  /*  mtuField(9) - int valid, int invalid 1, int invalid 2, float,
+      int string, float string, random string, undefined, null
+      isPPPoE(2) - true, false
+      total tests = 11 */
   test('validateMtu: mtuField(int valid), isPPPoE(true)', () => {
     let validator = new Validator();
     let mtuField = 1492;
@@ -15,9 +14,82 @@ describe('Validate functions used both in front and back end)', () => {
     expect(result).toHaveProperty('valid');
     expect(result.valid).toBe(true);
   });
-  /*  vlanField(7) - int valid, int invalid 1,
+  test('validateMtu: mtuField(int invalid 1), isPPPoE(true)', () => {
+    let validator = new Validator();
+    let mtuField = 442;
+    let isPPPoE = true;
+    let result = validator.validateMtu(mtuField, isPPPoE);
+    expect(result).toHaveProperty('valid');
+    expect(result.valid).toBe(false);
+  });
+  test('validateMtu: mtuField(int invalid 2), isPPPoE(true)', () => {
+    let validator = new Validator();
+    let mtuField = 2442;
+    let isPPPoE = true;
+    let result = validator.validateMtu(mtuField, isPPPoE);
+    expect(result).toHaveProperty('valid');
+    expect(result.valid).toBe(false);
+  });
+  test('validateMtu: mtuField(float), isPPPoE(true)', () => {
+    let validator = new Validator();
+    let mtuField = 1492.2941;
+    let isPPPoE = true;
+    let result = validator.validateMtu(mtuField, isPPPoE);
+    expect(result).toHaveProperty('valid');
+    expect(result.valid).toBe(false);
+  });
+  test('validateMtu: mtuField(int string), isPPPoE(true)', () => {
+    let validator = new Validator();
+    let mtuField = '1492';
+    let isPPPoE = true;
+    let result = validator.validateMtu(mtuField, isPPPoE);
+    expect(result).toHaveProperty('valid');
+    expect(result.valid).toBe(true);
+  });
+  test('validateMtu: mtuField(float string), isPPPoE(true)', () => {
+    let validator = new Validator();
+    let mtuField = '1492.2941';
+    let isPPPoE = true;
+    let result = validator.validateMtu(mtuField, isPPPoE);
+    expect(result).toHaveProperty('valid');
+    expect(result.valid).toBe(false);
+  });
+  test('validateMtu: mtuField(random string), isPPPoE(true)', () => {
+    let validator = new Validator();
+    let mtuField = 'boo';
+    let isPPPoE = true;
+    let result = validator.validateMtu(mtuField, isPPPoE);
+    expect(result).toHaveProperty('valid');
+    expect(result.valid).toBe(false);
+  });
+  test('validateMtu: mtuField(undefined), isPPPoE(true)', () => {
+    let validator = new Validator();
+    let mtuField;
+    let isPPPoE = true;
+    let result = validator.validateMtu(mtuField, isPPPoE);
+    expect(result).toHaveProperty('valid');
+    expect(result.valid).toBe(false);
+  });
+  test('validateMtu: mtuField(null), isPPPoE(true)', () => {
+    let validator = new Validator();
+    let mtuField = null;
+    let isPPPoE = true;
+    let result = validator.validateMtu(mtuField, isPPPoE);
+    expect(result).toHaveProperty('valid');
+    expect(result.valid).toBe(false);
+  });
+  /* ************************************************************** */
+  test('validateMtu: mtuField(valid), isPPPoE(false)', () => {
+    let validator = new Validator();
+    let mtuField = 1500;
+    let isPPPoE = false;
+    let result = validator.validateMtu(mtuField, isPPPoE);
+    expect(result).toHaveProperty('valid');
+    expect(result.valid).toBe(true);
+  });
+  /*  vlanField(9) - int valid, int invalid 1,
       int invalid 2, float, int string, float string,
-      non-numeric string, undefined, null
+      random string, undefined, null
       total tests = 9 */
   test('validateVlan: vlanField(int valid)', () => {
     let validator = new Validator();
@@ -61,7 +133,7 @@ describe('Validate functions used both in front and back end)', () => {
     expect(result).toHaveProperty('valid');
     expect(result.valid).toBe(false);
   });
-  test('validateVlan: vlanField(non-numeric string)', () => {
+  test('validateVlan: vlanField(random string)', () => {
     let validator = new Validator();
     let vlanField = 'boo';
     let result = validator.validateVlan(vlanField);
