41d7642706e08dd7e4acc040c53727fabb7bb411
lucasperovani <lucasperovani@gmail.com>
2023-02-15 14:58:59 -0300

    Improved tests.

--- test/unit/acs_device_info.test.js
+++ test/unit/acs_device_info.test.js
@@ -6,6 +6,8 @@ process.env.FLM_GENIE_IGNORED = 'TESTE!';
 const acsDeviceInfoController = require('../../controllers/acs_device_info');
 const tasksAPI = require('../../controllers/external-genieacs/tasks-api');
 
+const t = require('../../controllers/language').i18next.t;
+
 
 // controllers/acs_device_info
 describe('ACS Device Info Tests', () => {
@@ -31,7 +33,7 @@ describe('ACS Device Info Tests', () => {
 
 
     // Execute
-    acsDeviceInfoController.__testDelayExecutionGenie(
+    let result = await acsDeviceInfoController.__testDelayExecutionGenie(
       {acs_id: acsId},
       asyncFunc,
       0,
@@ -44,6 +46,11 @@ describe('ACS Device Info Tests', () => {
     expect(asyncFunc).not.toBeCalled();
     expect(setTimeout).not.toBeCalled();
     expect(genieSpy).not.toBeCalled();
+    expect(result.success).toBe(false);
+    expect(result.executed).toBe(false);
+    expect(result.message).toContain(
+      t('parametersError').replace('({{errorline}})', ''),
+    );
   });
 
 
@@ -62,7 +69,7 @@ describe('ACS Device Info Tests', () => {
 
 
     // Execute
-    acsDeviceInfoController.__testDelayExecutionGenie(
+    let result = await acsDeviceInfoController.__testDelayExecutionGenie(
       {acs_id: acsId},
       asyncFunc,
       -1,
@@ -75,6 +82,11 @@ describe('ACS Device Info Tests', () => {
     expect(asyncFunc).not.toBeCalled();
     expect(setTimeout).not.toBeCalled();
     expect(genieSpy).not.toBeCalled();
+    expect(result.success).toBe(false);
+    expect(result.executed).toBe(false);
+    expect(result.message).toContain(
+      t('parametersError').replace('({{errorline}})', ''),
+    );
   });
 
 
@@ -96,7 +108,7 @@ describe('ACS Device Info Tests', () => {
 
 
     // Execute
-    await acsDeviceInfoController.__testDelayExecutionGenie(
+    let result = await acsDeviceInfoController.__testDelayExecutionGenie(
       {acs_id: acsId},
       asyncFunc,
       5,
@@ -111,6 +123,9 @@ describe('ACS Device Info Tests', () => {
     expect(timeSpy.mock.calls[2][1]).toEqual(20000);
     expect(genieSpy).toHaveBeenCalledTimes(3);
     expect(asyncFunc).toHaveBeenCalledTimes(1);
+    expect(result.success).toBe(true);
+    expect(result.executed).toBe(true);
+    expect(result.message).toContain(t('Ok'));
   });
 
 
@@ -132,7 +147,7 @@ describe('ACS Device Info Tests', () => {
 
 
     // Execute
-    await acsDeviceInfoController.__testDelayExecutionGenie(
+    let result = await acsDeviceInfoController.__testDelayExecutionGenie(
       {acs_id: acsId},
       asyncFunc,
       5,
@@ -147,5 +162,121 @@ describe('ACS Device Info Tests', () => {
     expect(timeSpy.mock.calls[2][1]).toEqual(4000);
     expect(genieSpy).toHaveBeenCalledTimes(3);
     expect(asyncFunc).toHaveBeenCalledTimes(1);
+    expect(result.success).toBe(true);
+    expect(result.executed).toBe(true);
+    expect(result.message).toContain(t('Ok'));
+  });
+
+
+  // delayExecutionGenie - delayTime = 0
+  test('Validate delayExecutionGenie - delayTime = 0', async () => {
+    // Create a function to be passed
+    let asyncFunc = jest.fn(() => new Promise((resolve) => resolve()));
+    let acsId = '12345';
+
+
+    // Mocks
+    jest.useFakeTimers();
+    let timeSpy = jest.spyOn(global, 'setTimeout')
+      .mockImplementation(async (func) => await func());
+    let genieSpy = jest.spyOn(tasksAPI, 'getFromCollection')
+      .mockImplementationOnce(() => [])
+      .mockImplementationOnce(() => [])
+      .mockImplementation(() => [{_id: acsId}]);
+
+
+    // Execute
+    let result = await acsDeviceInfoController.__testDelayExecutionGenie(
+      {acs_id: acsId},
+      asyncFunc,
+      5,
+      0,
+    );
+
+
+    // Validate
+    expect(timeSpy).toHaveBeenCalledTimes(0);
+    expect(genieSpy).toHaveBeenCalledTimes(0);
+    expect(asyncFunc).toHaveBeenCalledTimes(0);
+    expect(result.success).toBe(false);
+    expect(result.executed).toBe(false);
+    expect(result.message).toContain(
+      t('parametersError').replace('({{errorline}})', ''),
+    );
+  });
+
+
+  // delayExecutionGenie - delayTime = -1
+  test('Validate delayExecutionGenie - delayTime = -1', async () => {
+    // Create a function to be passed
+    let asyncFunc = jest.fn(() => new Promise((resolve) => resolve()));
+    let acsId = '12345';
+
+
+    // Mocks
+    jest.useFakeTimers();
+    let timeSpy = jest.spyOn(global, 'setTimeout')
+      .mockImplementation(async (func) => await func());
+    let genieSpy = jest.spyOn(tasksAPI, 'getFromCollection')
+      .mockImplementationOnce(() => [])
+      .mockImplementationOnce(() => [])
+      .mockImplementation(() => [{_id: acsId}]);
+
+
+    // Execute
+    let result = await acsDeviceInfoController.__testDelayExecutionGenie(
+      {acs_id: acsId},
+      asyncFunc,
+      5,
+      -1,
+    );
+
+
+    // Validate
+    expect(timeSpy).toHaveBeenCalledTimes(0);
+    expect(genieSpy).toHaveBeenCalledTimes(0);
+    expect(asyncFunc).toHaveBeenCalledTimes(0);
+    expect(result.success).toBe(false);
+    expect(result.executed).toBe(false);
+    expect(result.message).toContain(
+      t('parametersError').replace('({{errorline}})', ''),
+    );
+  });
+
+
+  // delayExecutionGenie - no device
+  test('Validate delayExecutionGenie - no device', async () => {
+    // Create a function to be passed
+    let asyncFunc = jest.fn(() => new Promise((resolve) => resolve()));
+    let acsId = '12345';
+
+
+    // Mocks
+    jest.useFakeTimers();
+    let timeSpy = jest.spyOn(global, 'setTimeout')
+      .mockImplementation(async (func) => await func());
+    let genieSpy = jest.spyOn(tasksAPI, 'getFromCollection')
+      .mockImplementation(() => []);
+
+
+    // Execute
+    let result = await acsDeviceInfoController.__testDelayExecutionGenie(
+      {acs_id: acsId},
+      asyncFunc,
+      5,
+      1000,
+    );
+
+
+    // Validate
+    expect(timeSpy).toHaveBeenCalledTimes(5);
+    expect(timeSpy.mock.calls[0][1]).toEqual(1000);
+    expect(timeSpy.mock.calls[1][1]).toEqual(2000);
+    expect(timeSpy.mock.calls[2][1]).toEqual(4000);
+    expect(genieSpy).toHaveBeenCalledTimes(5);
+    expect(asyncFunc).toHaveBeenCalledTimes(0);
+    expect(result.success).toBe(true);
+    expect(result.executed).toBe(false);
+    expect(result.message).toContain(t('noDevicesFound'));
   });
 });
