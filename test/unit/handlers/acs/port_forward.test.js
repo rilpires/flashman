b8040cec5b8271fa82ca92317b2419cf48f6a6a2
Gaspare Bruno <gaspare@anlix.io>
2023-02-17 15:08:47 -0300

    Remove mongo connection from port_forward test All mongo procedures must be mocked in unit tests

--- test/unit/handlers/acs/port_forward.test.js
+++ test/unit/handlers/acs/port_forward.test.js
@@ -1,24 +1,9 @@
 require('../../../../bin/globals.js');
 const pfAcsHandlers = require(
   '../../../../controllers/handlers/acs/port_forward');
-const {MongoClient} = require('mongodb');
 process.env.FLM_GENIE_IGNORED = 'asd';
 
 describe('Controllers - Handlers - Port Forward', () => {
-  let connection;
-
-  beforeAll(async () => {
-    connection = await MongoClient.connect(global.__MONGO_URI__, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    await connection.db();
-  });
-
-  afterAll(async () => {
-    await connection.close();
-  });
-
   /* list of functions that may be mocked:
     http.request
     TasksAPI.getFromCollection
