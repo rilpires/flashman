9f2e3acbdd009a69c507afeeb8ffa5d2bfe9fcb8
lucasperovani <lucasperovani@gmail.com>
2023-03-21 08:41:22 -0300

    Removed unneeded describe in tests.

--- test/unit/handlers/utils.test.js
+++ test/unit/handlers/utils.test.js
@@ -10,133 +10,133 @@ describe('Utils Handler Tests', () => {
     jest.useRealTimers();
   });
 
-  describe('Test utils functions', () => {
-    // isMacValid
-    describe('isMacValid', () => {
-      // Invalid MAC with invalid character
-      test('Invalid MAC with invalid charactere', () => {
-        expect(utilHandlers.isMacValid('000000000000\n')).toBe(false);
-      });
+  // isMacValid
+  describe('isMacValid', () => {
+    // Invalid MAC with invalid character
+    test('Invalid MAC with invalid charactere', () => {
+      expect(utilHandlers.isMacValid('000000000000\n')).toBe(false);
+    });
 
-      // Invalid MAC
-      test('Invalid MAC with no invalid charactere', () => {
-        expect(utilHandlers.isMacValid('000000000000')).toBe(false);
-      });
+    // Invalid MAC
+    test('Invalid MAC with no invalid charactere', () => {
+      expect(utilHandlers.isMacValid('000000000000')).toBe(false);
+    });
 
-      // Normal MAC with invalid character
-      test('Valid MAC with invalid charactere', () => {
-        expect(utilHandlers.isMacValid('9C:A2:F4:5D:19:09\n')).toBe(false);
-      });
+    // Normal MAC with invalid character
+    test('Valid MAC with invalid charactere', () => {
+      expect(utilHandlers.isMacValid('9C:A2:F4:5D:19:09\n')).toBe(false);
+    });
 
-      // Valid MAC
-      test('Valid MAC with no invalid charactere', () => {
-        expect(utilHandlers.isMacValid('9C:A2:F4:5D:19:09')).toBe(true);
-      });
+    // Valid MAC
+    test('Valid MAC with no invalid charactere', () => {
+      expect(utilHandlers.isMacValid('9C:A2:F4:5D:19:09')).toBe(true);
+    });
+  });
+
+
+  // getMaskFromAddress
+  describe('getMaskFromAddress', () => {
+    // Invalid types and strings
+    test.each(
+      utils.common.TEST_PARAMETERS,
+    )('Invalid types and strings: %p', async (parameter) => {
+      // Execute
+      let result = utilHandlers.getMaskFromAddress(parameter);
+
+      // validate
+      expect(result).toBe(null);
     });
 
 
-    // getMaskFromAddress
-    describe('getMaskFromAddress', () => {
-      // Invalid types and strings
-      test.each(
-        utils.common.TEST_PARAMETERS,
-      )('Invalid types and strings: %p', async (parameter) => {
-        // Execute
-        let result = utilHandlers.getMaskFromAddress(parameter);
+    // Multiple /
+    test('Multiple /', () => {
+      // Execute
+      let result = utilHandlers.getMaskFromAddress('/////////////');
 
-        // validate
-        expect(result).toBe(null);
-      });
+      // validate
+      expect(result).toBe(null);
+    });
 
 
-      // Multiple /
-      test('Multiple /', () => {
-        // Execute
-        let result = utilHandlers.getMaskFromAddress('/////////////');
+    // Invalid number - Negative
+    test('Invalid number - Negative', () => {
+      // Execute
+      let result = utilHandlers.getMaskFromAddress('192.168.0.1/-1');
 
-        // validate
-        expect(result).toBe(null);
-      });
-
-
-      // Invalid number - Negative
-      test('Invalid number - Negative', () => {
-        // Execute
-        let result = utilHandlers.getMaskFromAddress('192.168.0.1/-1');
-
-        // validate
-        expect(result).toBe(null);
-      });
+      // validate
+      expect(result).toBe(null);
+    });
 
 
-      // Invalid number - Big
-      test('Invalid number - Big', () => {
-        // Execute
-        let result = utilHandlers.getMaskFromAddress('192.168.0.1/500');
+    // Invalid number - Big
+    test('Invalid number - Big', () => {
+      // Execute
+      let result = utilHandlers.getMaskFromAddress('192.168.0.1/500');
 
-        // validate
-        expect(result).toBe(null);
-      });
-
-
-      // Okay IPv4
-      test('Okay IPv4', () => {
-        // Execute
-        let result = utilHandlers.getMaskFromAddress('192.168.0.1/24');
-
-        // validate
-        expect(result).toBe('24');
-      });
+      // validate
+      expect(result).toBe(null);
+    });
 
 
-      // Okay IPv6
-      test('Okay IPv6', () => {
-        // Execute
-        let result = utilHandlers.getMaskFromAddress('2804:1234:5678::/56');
+    // Okay IPv4
+    test('Okay IPv4', () => {
+      // Execute
+      let result = utilHandlers.getMaskFromAddress('192.168.0.1/24');
 
-        // validate
-        expect(result).toBe('56');
-      });
-
-
-      // Limits - 0
-      test('Limits - 0', () => {
-        // Execute
-        let result = utilHandlers.getMaskFromAddress('192.168.0.1/0');
-
-        // validate
-        expect(result).toBe(null);
-      });
+      // validate
+      expect(result).toBe('24');
+    });
 
 
-      // Limits - 1
-      test('Limits - 1', () => {
-        // Execute
-        let result = utilHandlers.getMaskFromAddress('192.168.0.1/1');
+    // Okay IPv6
+    test('Okay IPv6', () => {
+      // Execute
+      let result = utilHandlers.getMaskFromAddress('2804:1234:5678::/56');
 
-        // validate
-        expect(result).toBe('1');
-      });
-
-
-      // Limits - 128
-      test('Limits - 128', () => {
-        // Execute
-        let result = utilHandlers.getMaskFromAddress('192.168.0.1/128');
-
-        // validate
-        expect(result).toBe('128');
-      });
+      // validate
+      expect(result).toBe('56');
+    });
 
 
-      // Limits - 129
-      test('Limits - 129', () => {
-        // Execute
-        let result = utilHandlers.getMaskFromAddress('192.168.0.1/129');
+    // Limits - 0
+    test('Limits - 0', () => {
+      // Execute
+      let result = utilHandlers.getMaskFromAddress('192.168.0.1/0');
 
-        // validate
-        expect(result).toBe(null);
-      });
+      // validate
+      expect(result).toBe(null);
+    });
+
+
+    // Limits - 1
+    test('Limits - 1', () => {
+      // Execute
+      let result = utilHandlers.getMaskFromAddress('192.168.0.1/1');
+
+      // validate
+      expect(result).toBe('1');
+    });
+
+
+    // Limits - 128
+    test('Limits - 128', () => {
+      // Execute
+      let result = utilHandlers.getMaskFromAddress('192.168.0.1/128');
+
+      // validate
+      expect(result).toBe('128');
+    });
+
+
+    // Limits - 129
+    test('Limits - 129', () => {
+      // Execute
+      let result = utilHandlers.getMaskFromAddress('192.168.0.1/129');
+
+      // validate
+      expect(result).toBe(null);
+    });
+  });
     });
   });
 });
