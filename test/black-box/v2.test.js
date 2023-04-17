6787ac649749566bbd49ae4991b20cb0d7b0aecf
cassiohg <cassiohg@gmail.com>
2023-04-17 19:43:43 -0300

    Created a promise that awaits the next diagnostic. It's possible to pass a name when waiting for next task and next diagnostic. Created a debug control function to turn some debug messages on or off. Importing formatXML from simulator instead defining it inside blackbox test. Using a device model local to the simulator instead of a model defined as a blackbox test asset.

--- test/black-box/v2.test.js
+++ test/black-box/v2.test.js
@@ -1,13 +1,15 @@
 // this test need to be run InBand (synchronous)
 require('../../bin/globals.js');
 const request = require('supertest');
-const {createSimulator, formatXML} = require('./cpe-tr069-simulator');
+const {createSimulator} = require('./cpe-tr069-simulator');
 
 
 describe('api_v2', () => {
   const basicAuthUser = 'admin';
   const basicAuthPass = 'flashman';
-  const deviceModelH199 = './test/assets/data_models/H199.csv';
+  // const deviceDataModel = './test/assets/data_models/H199.csv';
+  const deviceDataModel =
+    'device-00259E-EG8145V5-48575443A94196A5-2023-03-28T154335106Z';
   const flashmanHost = 'http://localhost:8000';
   const genieCwmpHost = 'http://localhost:57547';
 
@@ -15,7 +17,6 @@ describe('api_v2', () => {
 
   jest.setTimeout( 15*1000 );
 
-  const mac = 'FF:FF:FF:00:00:01';
   let simulator;
 
   // returns response from an http request, sent to flashman, with a user
@@ -43,41 +44,30 @@ describe('api_v2', () => {
       + `HTTP error: ${adminLogin.error}\n`,
       );
     }
-  });
-
-  // Device search
-  test('/api/v2/device/search - Before and After creation', async () => {
-    let res = await flashman('put', '/api/v2/device/search', {
-      filter_list: 'online',
-    });
-    expect(res.statusCode).toBe(200);
-    expect(res.header['content-type']).toContain('application/json');
-    expect(res.header['content-type']).toContain('charset=utf-8');
-    expect(res.body.success).toBe(true);
-    expect(res.body.status.onlinenum).toEqual(0);
-    expect(res.body.status.recoverynum).toEqual(0);
-    expect(res.body.status.offlinenum).toEqual(0);
-    expect(res.body.status.totalnum).toEqual(0);
 
     // Creating a device
-    simulator = createSimulator(genieCwmpHost, deviceModelH199, 1000, mac)
-    .on('started', () => {
-      console.log('*** simulator started');
-    }).on('ready', () => {
-      console.log('*** simulator ready');
-    }).on('requested', (request) => {
-      console.log(`- RECEIVED REQUEST BODY '${formatXML(request.body)}'.`);
-    }).on('response', (response) => {
-      console.log(`- RECEIVED RESPONSE BODY '${formatXML(response.body)}'.`);
-    }).on('sent', (request) => {
-      console.log(`- SENT BODY '${formatXML(request.body)}'.`);
-    }).on('task', (task) => {
-      console.log('- PROCESSED task', JSON.stringify(task, null, '  '));
+    let mac = 'FF:FF:FF:00:00:01';
+    simulator = createSimulator(genieCwmpHost, deviceDataModel, 1000, mac)
+    .debug({
+      beforeReady: false,
+      error: true,
+      xml: false,
+      requested: false,
+      response: false,
+      sent: false,
+      task: false,
+      diagnostic: false,
     });
     await simulator.start();
+  });
 
-    // Checking new result
-    res = await flashman('put', '/api/v2/device/search', {
+  afterAll(async () => {
+    await flashman('delete', `/api/v2/device/delete/${simulator.mac}`);
+    if (simulator) await simulator.shutDown();
+  });
+
+  test('/api/v2/device/search - After creation', async () => {
+    let res = await flashman('put', '/api/v2/device/search', {
       filter_list: 'online',
     });
     expect(res.statusCode).toBe(200);
@@ -96,26 +86,51 @@ describe('api_v2', () => {
         wifi_password: 'somepassword',
       },
     };
-    let res = await flashman('put', `/api/v2/device/update/${mac}`, update);
+    let res =
+      await flashman('put', `/api/v2/device/update/${simulator.mac}`, update);
+    // console.log('res.body', res.body)
     expect(res.statusCode).toBe(200);
     // eslint-disable-next-line guard-for-in
-    for (let field in update.content) {
+    for (const field in update.content) {
       expect(res.body[field]).toBe(update.content[field]);
     }
 
     // Waiting for simulator to process the task, respond and receive answer.
     await simulator.nextTask();
 
-    res = await flashman('get', `/api/v2/device/update/${mac}`);
+    res = await flashman('get', `/api/v2/device/update/${simulator.mac}`);
+    // console.log('res.body', res.body)
     expect(res.statusCode).toBe(200);
     // eslint-disable-next-line guard-for-in
-    for (let field in update.content) {
+    for (const field in update.content) {
       expect(res.body[field]).toBe(update.content[field]);
     }
   });
 
-  afterAll(async () => {
-    await flashman('delete', `/api/v2/device/delete/${mac}`);
-    if (simulator) await simulator.shutDown();
+  test('Firing ping diagnostic', async () => {
+    // starting ping diagnostic.
+    let res =
+      await flashman('put', `/api/v2/device/command/${simulator.mac}/ping`);
+    // console.log('res.body', res.body)
+    expect(res.statusCode).toBe(200);
+    expect(res.body.success).toBe(true);
+
+    // awaiting cpe to receive 4 diagnostics. it's always 4.
+    await simulator.nextDiagnostic(); // ping 1.
+    await simulator.nextDiagnostic(); // ping 2.
+    await simulator.nextDiagnostic(); // ping 3.
+    await simulator.nextDiagnostic(); // ping 4.
+    // waiting for Flashman to ask for final diagnostic result values.
+    await simulator.nextTask('GetParameterValues');
+    // console.log('waiting Flashman to process the last diagnostic')
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    res = await flashman('get', `/api/v2/device/update/${simulator.mac}`);
+    // console.log('res.body', res.body)
+    expect(res.statusCode).toBe(200);
+    expect(res.body.current_diagnostic.in_progress).toBe(false);
+    for (const result of res.body.pingtest_results) {
+      expect(result.completed).toBe(true);
+    }
   });
 });
