f4732d1576b64e49b8ea6a89a5d4569b0abaaabc
Thurler <gt@anlix.io>
2023-04-18 16:59:25 -0300

    Properly set new fields when they are sent from app

--- controllers/app_diagnostic_api.js
+++ controllers/app_diagnostic_api.js
@@ -84,6 +84,8 @@ const pushCertification = (arr, c, finished) => {
     routerModel: c.routerModel || '',
     routerVersion: c.routerVersion || '',
     routerRelease: c.routerRelease || '',
+    specificUsername: c.specificUser || '',
+    specificPassword: c.specificPasswd || '',
     localEpochTimestamp: c.timestamp || 0,
     didDiagnose: c.didDiagnose || false,
     diagnostic: convertDiagnostic(c.diagnostic),
