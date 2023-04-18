518080fad6dd96246c80f970350d1b30916b3f60
Thurler <gt@anlix.io>
2023-04-18 20:10:06 -0300

    Improved existing test logic and formatting

--- test/black-box/app_api.test.js
+++ test/black-box/app_api.test.js
@@ -1,56 +1,54 @@
 // this test need to be run InBand (synchronous)
 require('../../bin/globals.js');
 const request = require('supertest');
-// const utils = require('../utils.js');
 
-describe('/Deviceinfo/app', () => {
+describe('Diagnostic App API: /deviceinfo/app/diagnostic/', () => {
   const basicAuthUser = 'admin';
   const basicAuthPass = 'flashman';
   const flashmanHost = 'http://localhost:8000';
 
   jest.setTimeout( 15*1000 );
 
-  describe('/deviceinfo/app/diagnostic/login', () => {
+  const validateSuccessfulResponse = function(res) {
+    expect(res.statusCode).toBe(200);
+    expect(res.header['content-type']).toContain('application/json');
+    expect(res.header['content-type']).toContain('charset=utf-8');
+  };
+
+  describe('/login', () => {
     test('Check if specificWebLogin exists', async () => {
       let res = await request(flashmanHost)
         .post('/deviceinfo/app/diagnostic/login')
         .auth(basicAuthUser, basicAuthPass)
         .send({user: 'admin'});
-      expect(res.statusCode).toBe(200);
-      expect(res.header['content-type']).toContain('application/json');
-      expect(res.header['content-type']).toContain('charset=utf-8');
-      expect(typeof res.body.specificWebLogin)
-        .toMatch(/^boolean$/);
+      validateSuccessfulResponse(res);
+      expect(res.body).toHaveProperty('specificWebLogin');
+      expect(typeof res.body.specificWebLogin).toBe('boolean');
     });
   });
-  describe('/deviceinfo/app/diagnostic/certificate', () => {
+
+  describe('/certificate', () => {
     test('Check if specificWebLogin exists', async () => {
       let res = await request(flashmanHost)
         .post('/deviceinfo/app/diagnostic/certificate')
         .auth(basicAuthUser, basicAuthPass)
         .send({user: 'admin'});
-      expect(res.statusCode).toBe(200);
-      expect(res.header['content-type']).toContain('application/json');
-      expect(res.header['content-type']).toContain('charset=utf-8');
-      expect(typeof res.body.specificWebLogin)
-        .toMatch(/^boolean$/);
+      validateSuccessfulResponse(res);
+      expect(res.body).toHaveProperty('specificWebLogin');
+      expect(typeof res.body.specificWebLogin).toBe('boolean');
     });
   });
-  describe('/deviceinfo/app/diagnostic/verify', () => {
+
+  describe('/verify', () => {
     test('Check if specificWebLogin exists', async () => {
       let res = await request(flashmanHost)
         .post('/deviceinfo/app/diagnostic/verify')
         .auth(basicAuthUser, basicAuthPass)
         .send({mac: 'FF:FF:FF:FF:FF:FF'});
-      expect(res.statusCode).toBe(200);
-      expect(res.header['content-type']).toContain('application/json');
-      expect(res.header['content-type']).toContain('charset=utf-8');
-      expect(typeof res.body.onuConfig.specificWebLogin)
-        .toMatch(/^boolean$/);
+      validateSuccessfulResponse(res);
+      expect(res.body).toHaveProperty('onuConfig');
+      expect(res.body.onuConfig).toHaveProperty('specificWebLogin');
+      expect(typeof res.body.onuConfig.specificWebLogin).toBe('boolean');
     });
-  });
-
-  afterAll(async () => {
-    // Clean database? Close connections? Say goodbye?
   });
 });
