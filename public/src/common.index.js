54294ac25894333faffa0096d676e997e3a1d1bf
lucasperovani <lucasperovani@gmail.com>
2023-04-18 14:18:48 -0300

    Added sendRequest function to common.

--- public/src/common.index.js
+++ public/src/common.index.js
@@ -1,3 +1,7 @@
+/**
+ * @namespace public/src/common.index
+ */
+
 import 'bootstrap/js/src/index'; // Bootstrap animations
 import 'mdbootstrap/js/mdb'; // MDB animations
 
@@ -45,8 +49,69 @@ i18next
 const anlixDocumentReady = new function() {
   // array that will hold all callbacks to run after DOMContentLoaded event.
   this.readyCallbacks = [];
-  this.add = (f) => this.readyCallbacks.push(f); // adds callbacks to array.
-  this.start = () => this.readyCallbacks.forEach((f) => f()); // executes callbacks.
+};
+
+
+/**
+ * Send requests to the specified endpoint.
+ *
+ * @memberof public/src/common.index
+ *
+ * @param {String} endpoint - Endpoint to send request to.
+ * @param {String} type - `GET` or `POST`. The default is to call `GET` if the
+ * parameter is invalid.
+ * @param {String} deviceId - The `deviceId` to get the info from.
+ * @param {Function} successFunc - Callback function to be called when success.
+ * Arguments of the function:
+ * `successFunc(deviceId, response)`
+ *  - `deviceId` - The `deviceId` to get/set the info from.
+ *  - `response` - The response of the endpoint.
+ * @param {Function} errorFunc - Callback function to be called when an error
+ * happens. Arguments of the function:
+ * `errorFunc(deviceId, xhr, status, error)`
+ *  - `deviceId` - The `deviceId` to get/set the info from.
+ *  - `xhr`, `status`, `error` - See
+ * {@link https://api.jquery.com/jquery.ajax/|JQuery's API}
+ * @param {Object} data - The object to send to the endpoint.
+ *
+ * @see {@link https://api.jquery.com/jquery.ajax/|JQuery's API}
+ */
+const sendRequest = function(
+  endpoint,
+  type,
+  deviceId,
+  successFunc=null,
+  errorFunc=null,
+  data=null,
+) {
+  type = type.toUpperCase();
+
+  $.ajax({
+    url: endpoint,
+
+    method: (type === 'GET' || type === 'POST' ? type : 'GET'),
+
+    dataType: 'json',
+
+    data: (data !== null && data !== undefined ? data : ''),
+
+    contentType: (data !== null && data !== undefined ?
+      'application/json' : ''
+    ),
+
+    success: function(response) {
+      if (successFunc !== null && successFunc !== undefined) {
+        successFunc(deviceId, response);
+      }
+    },
+
+    error: function(xhr, status, error) {
+      if (errorFunc !== null && errorFunc !== undefined) {
+        errorFunc(deviceId, xhr, status, error);
+      }
+    },
+
+  });
 };
 
 export {anlixDocumentReady};
