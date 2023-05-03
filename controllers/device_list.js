fba4271897066f023474784e6b3f3cff7b6afa9c
lucasperovani <lucasperovani@gmail.com>
2023-05-03 17:05:42 -0300

    Improved get device function.

--- controllers/device_list.js
+++ controllers/device_list.js
@@ -5193,6 +5193,9 @@ deviceListController.getLeanDevice = function(filter) {
 };
 
 
+/**
+ * Queries and returns the first device that matches the field passed as a URL
+ * parameter.
  *
  * @memberof controllers/deviceList
  *
@@ -5204,9 +5207,55 @@ deviceListController.getLeanDevice = function(filter) {
  *  - `message` - The error message if any occurred.
  *  - `device`  - The device if found.
  */
-deviceListController.getByPPPoEUser = async function(request, response) {
+deviceListController.getDeviceByField = async function(request, response) {
+  let validator = new Validator();
+
+  const allFields = [
+    'PPPoEUsername', 'MAC', 'SerialTR069', 'ExternalReferenceData', 'WANMAC',
+  ];
+  const translateField = {
+    PPPoEUsername: {
+      field: 'pppoe_user',
+      validation: validator.validateUser,
+    },
+    MAC: {
+      field: '_id',
+      validation: validator.validateMac,
+    },
+    SerialTR069: {
+      field: 'serial_tr069',
+      validation: validator.validateUser,
+    },
+    ExternalReferenceData: {
+      field: 'external_reference.data',
+      validation: validator.validateDeviceName,
+    },
+    WANMAC: {
+      field: 'wan_bssid',
+      validation: validator.validateMac,
+    },
+  };
+
   // Validate missing information
-  if (!request || !request.params || !request.params.PPPoEUsername) {
+  if (!request || !request.params) {
+    let responseMessage = {
+      success: false,
+      message: t('requestError', {errorline: __line}),
+      device: {},
+    };
+
+    return response.status(500).json(responseMessage);
+  }
+
+
+  // Get the parameter in use
+  let paramIndex = allFields.findIndex((field) => request.params[field]);
+
+  // Check if parameter exists
+  if (
+    paramIndex < 0 || !request.params[allFields[paramIndex]] ||
+    typeof request.params[allFields[paramIndex]] !== 'string'
+  ) {
     let responseMessage = {
       success: false,
       message: t('fieldNotFound', {errorline: __line}),
@@ -5216,40 +5265,42 @@ deviceListController.getByPPPoEUser = async function(request, response) {
     return response.status(500).json(responseMessage);
   }
 
+  let paramName = allFields[paramIndex];
+  let fieldName = translateField[paramName].field;
+  let checkFunc = translateField[paramName].validation;
+  let param = request.params[paramName];
+
+
+  // Check if parameter is valid
+  let valid = checkFunc(param);
+  if (!valid.valid) {
+    let responseMessage = {
+      success: false,
+      message: valid.err,
+      device: {},
+    };
+
+    return response.status(500).json(responseMessage);
+  }
+
+
+  // Build the query
+  let query = {};
+  query[fieldName] = param;
+
 
   // Get the device
   let device = null;
   try {
-    device = await DeviceModel.findOne(
-      // Search by PPPoE usernam
-      {pppoe_user: request.params.PPPoEUsername},
-
-      // Reduce fields
-      {
-        alt_uid_tr069: false, acs_sync_loops: false, data_collecting: false,
-        pppoe_password: false, pon_signal_measure: false, app_password: false,
-        lan_devices: false, wrong_port_mapping: false, ap_survey: false,
-        upnp_requests: false, mesh_slaves: false, mesh_id: false,
-        mesh_key: false, bssid_mesh2: false, bssid_mesh5: false,
-        mesh_routers: false, do_update: false, do_update_parameters: false,
-        do_update_status: false, mesh_next_to_update: false,
-        mesh_onlinedevs_remaining: false, mesh_update_remaining: false,
-        mqtt_secret: false, mqtt_secret_bypass: false, firstboot_log: false,
-        lastboot_log: false, apps: false, pending_app_secret: false,
-        forward_index: false, blocked_devices_index: false,
-        upnp_devices_index: false, ping_hosts: false, pingtest_results: false,
-        wan_bytes: false, speedtest_results: false, last_speedtest_error: false,
-        current_diagnostic: false, stop_coordinates_update: false,
-        web_admin_password: false, do_tr069_update_connection_login: false,
-        traceroute_max_hops: false, traceroute_number_probes: false,
-        traceroute_max_wait: false, traceroute_results: false,
-        // Extra fields
-        temp_command_trap: false, current_speedtest: false, __v: false,
-      },
-    ).lean();
+    // Search by PPPoE username and get the slim version of the device
+    device = await deviceListController.getLeanDevice(query);
 
   // Error from mongo
   } catch (error) {
+    console.log(
+      'Failed to find device in getDeviceByField with error: ' + error,
+    );
+
     let responseMessage = {
       success: false,
       message: t('databaseFindError', {errorline: __line}),
@@ -5258,6 +5309,7 @@ deviceListController.getByPPPoEUser = async function(request, response) {
 
     return response.status(500).json(responseMessage);
   }
+
 
   // if could not find the device
   if (!device) {
