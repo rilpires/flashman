43f83dea7b0d926dbbdf504faa1be6dd5b05f5b5
lucasperovani <lucasperovani@gmail.com>
2023-05-03 17:06:30 -0300

    Added new routes and improving docs.

--- routes/api/v3.js
+++ routes/api/v3.js
@@ -60,6 +60,8 @@ router.use(
  *      description: Query the first device that has the same PPPoE username
  *        passed as a URL parameter.
  *
+ *      tags: ['Get Device']
+ *
  *      parameters:
  *        - in: path
  *          name: PPPoEUsername
@@ -92,7 +94,82 @@ router.use(
  *                  device:
  *                    type: object
  *                    description: The device if could found one.
- *                    example: {_id: 'AA:BB:CC:DD:EE:FF'}
+ *                    properties:
+ *                      _id:
+ *                        type: string
+ *                        description: The MAC address of the device.
+ *                        example: 'AA:BB:CC:DD:EE:FF'
+ *
+ *                      resources_usage:
+ *                        type: object
+ *                        description: The CPU and Memory usage of the device.
+ *                        properties:
+ *                          cpu_usage:
+ *                            type: integer
+ *                            description: The CPU usage in percentage.
+ *                            example: 57
+ *
+ *                          memory_usage:
+ *                            type: integer
+ *                            description: The Memory usage in percentage.
+ *                            example: 34
+ *
+ *                      version:
+ *                        type: string
+ *                        description: The firmware version.
+ *                        example: '0.35.3'
+ *
+ *                      wifi_state:
+ *                        type: integer
+ *                        description: If the 2.4GHz Wi-Fi is turned on or not.
+ *                          0 means turned off and 1 means turned on.
+ *                        example: 1
+ *
+ *                      wifi_hidden:
+ *                        type: integer
+ *                        description: If the 2.4GHz Wi-Fi is hidden or not.
+ *                        example: 0
+ *
+ *                      wifi_power:
+ *                        type: integer
+ *                        description: The 2.4GHz Wi-Fi power.
+ *                        example: 100
+ *
+ *                      wifi_is_5ghz_capable:
+ *                        type: boolean
+ *                        description: If the router supports 5GHz Wi-Fi.
+ *                        example: true
+ *
+ *                      wifi_state_5ghz:
+ *                        type: integer
+ *                        description: If the 5GHz Wi-Fi is turned on or not.
+ *                          0 means turned off and 1 means turned on.
+ *                        example: 1
+ *
+ *                      wifi_hidden_5ghz:
+ *                        type: integer
+ *                        description: If the 5GHz Wi-Fi is hidden or not.
+ *                        example: 0
+ *
+ *                      wifi_power_5ghz:
+ *                        type: integer
+ *                        description: The 5GHz Wi-Fi power.
+ *                        example: 100
+ *
+ *                      mesh_mode:
+ *                        type: integer
+ *                        description: The mode of the mesh (
+ *                          0 -> Disabled mesh;
+ *                          1 -> Cable only;
+ *                          2 -> Wi-Fi 2.4Ghz as backhaul;
+ *                          3 -> Wi-Fi 5Ghz as backhaul;
+ *                          4 -> Use both Wi-Fi. )
+ *                        example: 0
+ *
+ *                      mesh_father:
+ *                        type: string
+ *                        description: The MAC address of the .
+ *                        example: 100
  *
  *        401:
  *          description: Authentication information is missing or invalid. Send
@@ -114,7 +191,7 @@ router.use(
  *                  message:
  *                    type: string
  *                    description: The error message if one occurred.
- *                    example: 'No Device Found'
+ *                    example: 'Error accessing database (xxxx)'
  *                  device:
  *                    type: object
  *                    description: The device if could found one.
@@ -122,7 +199,353 @@ router.use(
  */
 router.get(
   '/device/getByPPPoEUser/:PPPoEUsername',
-  deviceListController.getByPPPoEUser,
+  deviceListController.getDeviceByField,
+);
+
+
+/**
+ * Get a device by it's MAC address. It's an API route to query the first
+ * device that has the same MAC address as passed in URL as a parameter.
+ *
+ * @memberof routes/api/v3
+ *
+ * @param {String} MAC - The MAC address of the device to be returned.
+ *
+ * @return {Model} The first device that matched the `MAC`.
+ *
+ * @openapi
+ *  /api/v3/device/getByMAC/{MAC}:
+ *    get:
+ *      summary: Get a device by it's MAC address.
+ *
+ *      description: Query the first device that has the same MAC address
+ *        passed as a URL parameter.
+ *
+ *      tags: ['Get Device']
+ *
+ *      parameters:
+ *        - in: path
+ *          name: MAC
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: The MAC address of the device to be returned.
+ *
+ *      security:
+ *        - basicAuth: []
+ *
+ *      responses:
+ *        200:
+ *          description: The first device found that matches the `MAC`
+ *            or an empty device with success false as it could not find the
+ *            device.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  success:
+ *                    type: boolean
+ *                    description: If could find the device or not.
+ *                    example: true
+ *                  message:
+ *                    type: string
+ *                    description: The error message if one occurred.
+ *                    example: 'OK'
+ *                  device:
+ *                    type: object
+ *                    description: The device if could found one.
+ *                    example: {_id: 'AA:BB:CC:DD:EE:FF'}
+ *
+ *        401:
+ *          description: Authentication information is missing or invalid. Send
+ *            the correct user and password as basic auth.
+ *
+ *        500:
+ *          description: An internal error happenned. It can be caused by the
+ *            missing `MAC` parameter, an invalid request or if could not read
+ *            the database.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  success:
+ *                    type: boolean
+ *                    description: If could find the device or not.
+ *                    example: false
+ *                  message:
+ *                    type: string
+ *                    description: The error message if one occurred.
+ *                    example: 'Error accessing database (xxxx)'
+ *                  device:
+ *                    type: object
+ *                    description: The device if could found one.
+ *                    example: {}
+ */
+router.get(
+  '/device/getByMAC/:MAC',
+  deviceListController.getDeviceByField,
+);
+
+
+/**
+ * Get a device by it's TR069 Serial. It's an API route to query the first
+ * device that has the same TR069 Serial as passed in URL as a parameter.
+ *
+ * @memberof routes/api/v3
+ *
+ * @param {String} SerialTR069 - The TR069 Serial of the device to be returned.
+ *
+ * @return {Model} The first device that matched the `SerialTR069`.
+ *
+ * @openapi
+ *  /api/v3/device/getBySerialTR069/{SerialTR069}:
+ *    get:
+ *      summary: Get a device by it's TR069 Serial.
+ *
+ *      description: Query the first device that has the same TR069 Serial
+ *        passed as a URL parameter.
+ *
+ *      tags: ['Get Device']
+ *
+ *      parameters:
+ *        - in: path
+ *          name: SerialTR069
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: The TR069 Serial of the device to be returned.
+ *
+ *      security:
+ *        - basicAuth: []
+ *
+ *      responses:
+ *        200:
+ *          description: The first device found that matches the `SerialTR069`
+ *            or an empty device with success false as it could not find the
+ *            device.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  success:
+ *                    type: boolean
+ *                    description: If could find the device or not.
+ *                    example: true
+ *                  message:
+ *                    type: string
+ *                    description: The error message if one occurred.
+ *                    example: 'OK'
+ *                  device:
+ *                    type: object
+ *                    description: The device if could found one.
+ *                    example: {_id: 'AA:BB:CC:DD:EE:FF'}
+ *
+ *        401:
+ *          description: Authentication information is missing or invalid. Send
+ *            the correct user and password as basic auth.
+ *
+ *        500:
+ *          description: An internal error happenned. It can be caused by the
+ *            missing `SerialTR069` parameter, an invalid request or if could
+ *            not read the database.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  success:
+ *                    type: boolean
+ *                    description: If could find the device or not.
+ *                    example: false
+ *                  message:
+ *                    type: string
+ *                    description: The error message if one occurred.
+ *                    example: 'Error accessing database (xxxx)'
+ *                  device:
+ *                    type: object
+ *                    description: The device if could found one.
+ *                    example: {}
+ */
+router.get(
+  '/device/getBySerialTR069/:SerialTR069',
+  deviceListController.getDeviceByField,
+);
+
+
+/**
+ * Get a device by it's External Reference. It's an API route to query the first
+ * device that has the same External Reference as passed in URL as a parameter.
+ *
+ * @memberof routes/api/v3
+ *
+ * @param {String} ExternalReferenceData - The External Reference of the device
+ * to be returned.
+ *
+ * @return {Model} The first device that matched the `ExternalReferenceData`.
+ *
+ * @openapi
+ *  /api/v3/device/getByExternalReferenceData/{ExternalReferenceData}:
+ *    get:
+ *      summary: Get a device by it's External Reference.
+ *
+ *      description: Query the first device that has the same External Reference
+ *        passed as a URL parameter.
+ *
+ *      tags: ['Get Device']
+ *
+ *      parameters:
+ *        - in: path
+ *          name: ExternalReferenceData
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: The External Reference of the device to be returned.
+ *
+ *      security:
+ *        - basicAuth: []
+ *
+ *      responses:
+ *        200:
+ *          description: The first device found that matches the
+ *            `ExternalReferenceData` or an empty device with success false as
+ *             it could not find the device.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  success:
+ *                    type: boolean
+ *                    description: If could find the device or not.
+ *                    example: true
+ *                  message:
+ *                    type: string
+ *                    description: The error message if one occurred.
+ *                    example: 'OK'
+ *                  device:
+ *                    type: object
+ *                    description: The device if could found one.
+ *                    example: {_id: 'AA:BB:CC:DD:EE:FF'}
+ *
+ *        401:
+ *          description: Authentication information is missing or invalid. Send
+ *            the correct user and password as basic auth.
+ *
+ *        500:
+ *          description: An internal error happenned. It can be caused by the
+ *            missing `ExternalReferenceData` parameter, an invalid request or
+ *            if could not read
+ *            the database.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  success:
+ *                    type: boolean
+ *                    description: If could find the device or not.
+ *                    example: false
+ *                  message:
+ *                    type: string
+ *                    description: The error message if one occurred.
+ *                    example: 'Error accessing database (xxxx)'
+ *                  device:
+ *                    type: object
+ *                    description: The device if could found one.
+ *                    example: {}
+ */
+router.get(
+  '/device/getByExternalReferenceData/:ExternalReferenceData',
+  deviceListController.getDeviceByField,
+);
+
+
+/**
+ * Get a device by it's WAN MAC address. It's an API route to query the first
+ * device that has the same WAN MAC address as passed in URL as a parameter.
+ *
+ * @memberof routes/api/v3
+ *
+ * @param {String} WANMAC - The WAN MAC address of the device to be returned.
+ *
+ * @return {Model} The first device that matched the `WANMAC`.
+ *
+ * @openapi
+ *  /api/v3/device/getByWANMAC/{WANMAC}:
+ *    get:
+ *      summary: Get a device by it's WAN MAC address.
+ *
+ *      description: Query the first device that has the same WAN MAC address
+ *        passed as a URL parameter.
+ *
+ *      tags: ['Get Device']
+ *
+ *      parameters:
+ *        - in: path
+ *          name: WANMAC
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: The WAN MAC address of the device to be returned.
+ *
+ *      security:
+ *        - basicAuth: []
+ *
+ *      responses:
+ *        200:
+ *          description: The first device found that matches the `WANMAC`
+ *            or an empty device with success false as it could not find the
+ *            device.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  success:
+ *                    type: boolean
+ *                    description: If could find the device or not.
+ *                    example: true
+ *                  message:
+ *                    type: string
+ *                    description: The error message if one occurred.
+ *                    example: 'OK'
+ *                  device:
+ *                    type: object
+ *                    description: The device if could found one.
+ *                    example: {_id: 'AA:BB:CC:DD:EE:FF'}
+ *
+ *        401:
+ *          description: Authentication information is missing or invalid. Send
+ *            the correct user and password as basic auth.
+ *
+ *        500:
+ *          description: An internal error happenned. It can be caused by the
+ *            missing `WANMAC` parameter, an invalid request or if could not
+ *            read the database.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  success:
+ *                    type: boolean
+ *                    description: If could find the device or not.
+ *                    example: false
+ *                  message:
+ *                    type: string
+ *                    description: The error message if one occurred.
+ *                    example: 'Error accessing database (xxxx)'
+ *                  device:
+ *                    type: object
+ *                    description: The device if could found one.
+ *                    example: {}
+ */
+router.get(
+  '/device/getByWANMAC/:WANMAC',
+  deviceListController.getDeviceByField,
 );
 
 
