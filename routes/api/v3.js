51ded2a1b50b0f433848634771dd2a9047060319
lucasperovani <lucasperovani@gmail.com>
2023-05-03 11:21:13 -0300

    Added CORS.

--- routes/api/v3.js
+++ routes/api/v3.js
@@ -5,7 +5,29 @@ const router = express.Router();
 const authController = require('../../controllers/auth');
 
 
-// Ensure that the user has access to API routes in this file
+// Enable CORS to all V3 routes as it is necessary for Swagger to send commands.
+router.use((_request, response, next) => {
+  // Set the headers
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Authorization, Accept',
+  );
+
+  next();
+});
+
+
+/**
+ * This function is necessary to allow getting header parameters when all
+ * endpoints need authentication due to `CORS` issues.
+ */
+router.options('/*', function(_request, response) {
+  response.sendStatus(200);
+});
+
+
+// Ensure that the user has access to API routes in this file.
 router.use(
   authController.ensureAPIAccess,
 );
