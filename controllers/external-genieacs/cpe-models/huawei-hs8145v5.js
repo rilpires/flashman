0ff630793a43cd291fc0ef3ac824924c2707f699
Gaspare Bruno <gaspare@anlix.io>
2023-06-06 18:48:18 -0300

    Correct model 8145

--- controllers/external-genieacs/cpe-models/huawei-hs8145v5.js
+++ controllers/external-genieacs/cpe-models/huawei-hs8145v5.js
@@ -2,7 +2,7 @@ const basicCPEModel = require('./base-model');
 
 let huaweiModel = Object.assign({}, basicCPEModel);
 
-huaweiModel.identifier = {vendor: 'Huawei', model: 'HS8545V5'};
+huaweiModel.identifier = {vendor: 'Huawei', model: 'HS8145V5'};
 
 huaweiModel.modelPermissions = function() {
   let permissions = basicCPEModel.modelPermissions();
